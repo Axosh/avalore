c472f967d47c328d067d265907edc5b05d18ea70
Axosh <axosh.games@gmail.com>
2020-01-28 08:49:29 -0600

    Got some basic broadcast messages working in panorama, started working on modifying abilities

--- avalore/game/scripts/vscripts/addon_game_mode.lua
+++ avalore/game/scripts/vscripts/addon_game_mode.lua
@@ -1,11 +1,24 @@
--- Generated from template
+--[[
+Avalore Game Mode
+]]
 
 _G.nCOUNTDOWNTIMER = 2401
+_G.Temp = false
+
+---------------------------------------------------------------------------
+-- CAvaloreGameMode class
+---------------------------------------------------------------------------
 
 if CAvaloreGameMode == nil then
 	_G.CAvaloreGameMode = class({})
-	--CAvaloreGameMode = class({})
 end
+
+---------------------------------------------------------------------------
+-- Required .lua files
+---------------------------------------------------------------------------
+--require( "events" )
+--require( "items" )
+require( "utility_functions" )
 
 function Precache( context )
 	--[[
@@ -30,7 +43,12 @@ end
 function CAvaloreGameMode:InitGameMode()
 	print( "Avalore is loaded." )
 	GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 2 )
-	nCOUNTDOWNTIMER = 2401
+	_G.nCOUNTDOWNTIMER = 2401
+	print( "CAvaloreGameMode:InitGameMode()" )
+	self.countdownEnabled = true
+	GameRules:SetPreGameTime( 10 )
+	GameRules:SetStrategyTime( 0.0 )
+	GameRules:SetShowcaseTime( 0.0 )
 end
 
 -- Evaluate the state of the game
@@ -40,8 +58,44 @@ function CAvaloreGameMode:OnThink()
 	--elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
 	--	return nil
 	--end
+	curr_gametime = GameRules:GetGameTime()
 	if self.countdownEnabled == true then
 		CountdownTimer()
+		--_G.nCOUNTDOWNTIMER = _G.nCOUNTDOWNTIMER - 1;
+		print("Countdown = " .. tostring(_G.nCOUNTDOWNTIMER))
+	end
+	print("Gametime = " .. tostring(curr_gametime))
+	print("_G.Temp = " .. tostring(_G.Temp))
+
+	if curr_gametime > 20 and _G.Temp == false then
+		--BroadcastMessage("Test Test Test", 3)
+		local broadcast_obj =
+		{
+			msg = "#Helloworld",
+			time = 5
+		}
+		local test = 
+		{
+			msg = "test"
+		}
+		CustomGameEventManager:Send_ServerToAllClients( "broadcast_message", broadcast_obj )
+		--CustomGameEventManager:Send_ServerToAllClients( "test", test )
+		EmitGlobalSound( "DOTA_Item.Refresher.Activate" )
+		_G.Temp = true
+	end
+	-- game more: wisps
+	--0-600 = wisp
+	--600-1200 = koth
+	--1200-1800 = boss
+	--1800-2400 = waves
+	if curr_gametime > 1800 then
+		print("waves")
+	elseif curr_gametime > 1200 then
+		print("boss")
+	elseif curr_gametime > 600 then 
+		print("koth")
+	elseif curr_gametime > 0 then
+		print("capture")
 	end
 	return 1
 end
