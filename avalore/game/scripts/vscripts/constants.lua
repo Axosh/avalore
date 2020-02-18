1b457b1ddf9d8473f812df45894eeb7cbfeddfb4
Axosh <axosh.games@gmail.com>
2020-02-18 01:04:58 -0600

    - utilized new constants class/file in spawners - implemented shelf (sort of a lane) spawners - updated neutral camp types - named all tower, rax, fountain, and ancient entities - added backdoor protection for towers, rax, and ancients - set tower location properties - set rax location properties

--- avalore/game/scripts/vscripts/constants.lua
+++ avalore/game/scripts/vscripts/constants.lua
@@ -22,12 +22,79 @@ Constants.TIME_ROUND_4_START = 1800  --30 min
 -- SPAWNER KEY NAMES
 -- ==================================
 
-Constants.KEY_RADIANT_TOP   = "Radiant_Top"
-Constants.KEY_RADIANT_MIDA  = "Radiant_MidA"
-Constants.KEY_RADIANT_MIDB  = "Radiant_MidB"
-Constants.KEY_RADIANT_BOT   = "Radiant_Bot"
+Constants.KEY_RADIANT_TOP       = "Radiant_Top"
+Constants.KEY_RADIANT_MIDA      = "Radiant_MidA"
+Constants.KEY_RADIANT_MIDB      = "Radiant_MidB"
+Constants.KEY_RADIANT_BOT       = "Radiant_Bot"
+Constants.KEY_RADIANT_SHELFTOP  = "Radiant_ShelfTop"
+Constants.KEY_RADIANT_SHELFBOT  = "Radiant_ShelfBot"
 
-Constants.KEY_DIRE_TOP      = "Dire_Top"
-Constants.KEY_DIRE_MIDA     = "Dire_MidA"
-Constants.KEY_DIRE_MIDB     = "Dire_MidB"
-Constants.KEY_DIRE_BOT      = "Dire_Bot"
+Constants.KEY_DIRE_TOP          = "Dire_Top"
+Constants.KEY_DIRE_MIDA         = "Dire_MidA"
+Constants.KEY_DIRE_MIDB         = "Dire_MidB"
+Constants.KEY_DIRE_BOT          = "Dire_Bot"
+Constants.KEY_DIRE_SHELFTOP     = "Dire_ShelfTop"
+Constants.KEY_DIRE_SHELFBOT     = "Dire_ShelfBot"
+
+--[[
+    RADIANT BASE ENTITIES
+    rax_ranged_radiant_mid
+    rax_melee_radiant_mid
+
+    rax_ranged_radiant_top
+    rax_melee_radiant_top
+
+    rax_ranged_radiant_bot
+    rax_melee_radiant_bot
+
+    ancient_radiant
+    fountain_radiant
+
+    RADIANT TOWER ENTITIES
+
+    tower_radiant_top_1
+    tower_radiant_top_2
+    tower_radiant_top_3
+    tower_radiant_top_4
+
+    tower_radiant_bot_1
+    tower_radiant_bot_2
+    tower_radiant_bot_3
+    tower_radiant_bot_4
+
+    tower_radiant_mid_1
+    tower_radiant_mid_2
+    tower_radiant_mid_3
+--]]
+
+--[[
+    DIRE BASE ENTITIES
+
+    rax_ranged_dire_mid
+    rax_melee_dire_mid
+
+    rax_ranged_dire_bot
+    rax_melee_dire_bot
+
+    rax_ranged_dire_top
+    rax_melee_dire_top
+
+    ancient_dire
+    fountain_dire
+
+    DIRE TOWER ENTITIES
+
+    tower_dire_top_2
+    tower_dire_top_1
+    tower_dire_top_3
+    tower_dire_top_4
+
+    tower_dire_bot_1
+    tower_dire_bot_2
+    tower_dire_bot_3
+    tower_dire_bot_4
+
+    tower_dire_mid_1
+    tower_dire_mid_2
+    tower_dire_mid_3
+--]]
--- avalore/game/scripts/vscripts/spawners.lua
+++ avalore/game/scripts/vscripts/spawners.lua
@@ -8,31 +8,40 @@ require("constants")
 -- a reference to them when working with it later
 function Spawners:Init()
     -- GameTime in seconds to start splitting the middle wave around the center (mostly for debug)
-    Spawners.iSplitTime = 60 --Constants.TIME_ROUND_2_START
+    Spawners.iSplitTime = Constants.TIME_ROUND_2_START
+    Spawners.iShelfInterval = 120 
 
     -- Spawning Handles (Entities)
-    Spawners.h_RadiantSpawn_Top     = Entities:FindByName(nil, "spawner_good_top")
-    Spawners.h_RadiantSpawn_Mid_A   = Entities:FindByName(nil, "spawner_good_mid")
-    Spawners.h_RadiantSpawn_Mid_B   = Entities:FindByName(nil, "spawner_good_midb")
-    Spawners.h_RadiantSpawn_Bot     = Entities:FindByName(nil, "spawner_good_bot")
+    Spawners.h_RadiantSpawn_Top             = Entities:FindByName(nil, "spawner_good_top")
+    Spawners.h_RadiantSpawn_Mid_A           = Entities:FindByName(nil, "spawner_good_mid")
+    Spawners.h_RadiantSpawn_Mid_B           = Entities:FindByName(nil, "spawner_good_midb")
+    Spawners.h_RadiantSpawn_Bot             = Entities:FindByName(nil, "spawner_good_bot")
+    Spawners.h_RadiantSpawn_ShelfTop        = Entities:FindByName(nil, "spawner_good_shelf_top")
+    Spawners.h_RadiantSpawn_ShelfBot        = Entities:FindByName(nil, "spawner_good_shelf_bot")
 
-    Spawners.h_DireSpawn_Top     = Entities:FindByName(nil, "spawner_bad_top")
-    Spawners.h_DireSpawn_Mid_A   = Entities:FindByName(nil, "spawner_bad_mid")
-    Spawners.h_DireSpawn_Mid_B   = Entities:FindByName(nil, "spawner_bad_midb")
-    Spawners.h_DireSpawn_Bot     = Entities:FindByName(nil, "spawner_bad_bot")
+    Spawners.h_DireSpawn_Top                = Entities:FindByName(nil, "spawner_bad_top")
+    Spawners.h_DireSpawn_Mid_A              = Entities:FindByName(nil, "spawner_bad_mid")
+    Spawners.h_DireSpawn_Mid_B              = Entities:FindByName(nil, "spawner_bad_midb")
+    Spawners.h_DireSpawn_Bot                = Entities:FindByName(nil, "spawner_bad_bot")
+    Spawners.h_DireSpawn_ShelfTop           = Entities:FindByName(nil, "spawner_bad_shelf_top")
+    Spawners.h_DireSpawn_ShelfBot           = Entities:FindByName(nil, "spawner_bad_shelf_bot")
 
     -- Initial Path Node Handles (Entities)
-    Spawners.h_RadiantInit_Top          = Entities:FindByName(nil, "radiant_path_top_1")
-    Spawners.h_RadiantInit_Mid_Main     = Entities:FindByName(nil, "radiant_path_start")
-    Spawners.h_RadiantInit_Mid_AltA     = Entities:FindByName(nil, "radiant_path_mid_1a")
-    Spawners.h_RadiantInit_Mid_AltB     = Entities:FindByName(nil, "radiant_path_mid_1b")
-    Spawners.h_RadiantInit_Bot          = Entities:FindByName(nil, "radiant_path_bot_1")
+    Spawners.h_RadiantInit_Top              = Entities:FindByName(nil, "radiant_path_top_1")
+    Spawners.h_RadiantInit_Mid_Main         = Entities:FindByName(nil, "radiant_path_start")
+    Spawners.h_RadiantInit_Mid_AltA         = Entities:FindByName(nil, "radiant_path_mid_1a")
+    Spawners.h_RadiantInit_Mid_AltB         = Entities:FindByName(nil, "radiant_path_mid_1b")
+    Spawners.h_RadiantInit_Bot              = Entities:FindByName(nil, "radiant_path_bot_1")
+    Spawners.h_RadiantInit_ShelfTop         = Entities:FindByName(nil, "radiant_path_shelf_top1")
+    Spawners.h_RadiantInit_ShelfBot         = Entities:FindByName(nil, "radiant_path_shelf_bot1")
 
-    Spawners.h_DireInit_Top          = Entities:FindByName(nil, "dire_path_top_1")
-    Spawners.h_DireInit_Mid_Main     = Entities:FindByName(nil, "dire_path_start")
-    Spawners.h_DireInit_Mid_AltA     = Entities:FindByName(nil, "dire_path_mid_1a")
-    Spawners.h_DireInit_Mid_AltB     = Entities:FindByName(nil, "dire_path_mid_1b")
-    Spawners.h_DireInit_Bot          = Entities:FindByName(nil, "dire_path_bot_1")
+    Spawners.h_DireInit_Top                 = Entities:FindByName(nil, "dire_path_top_1")
+    Spawners.h_DireInit_Mid_Main            = Entities:FindByName(nil, "dire_path_start")
+    Spawners.h_DireInit_Mid_AltA            = Entities:FindByName(nil, "dire_path_mid_1a")
+    Spawners.h_DireInit_Mid_AltB            = Entities:FindByName(nil, "dire_path_mid_1b")
+    Spawners.h_DireInit_Bot                 = Entities:FindByName(nil, "dire_path_bot_1")
+    Spawners.h_DireInit_ShelfTop            = Entities:FindByName(nil, "dire_path_shelf_top1")
+    Spawners.h_DireInit_ShelfBot            = Entities:FindByName(nil, "dire_path_shelf_bot1")
 
     -- Create Spawn Configs to Loop Through
     -- Key = Location; Value = {Spawner, FirstWaypoint, MeleeCreepToUse, RangedCreepToUse, Siege, Team}
@@ -40,15 +49,19 @@ function Spawners:Init()
     -- but they can be updated in other places (e.g. on rax death to change the creep to super/mega)
     -- start the alt routes spawners as nil so they don't spawn until at 10 min they get forced to start up
     Spawners.SpawnConfigs = {}
-    Spawners.SpawnConfigs[Constants.KEY_RADIANT_TOP]   = {Spawner = self.h_RadiantSpawn_Top,   FirstWaypoint = self.h_RadiantInit_Top,      Melee = "npc_dota_creep_goodguys_melee", Ranged = "npc_dota_creep_goodguys_ranged", Siege = "npc_dota_goodguys_siege", Team = DOTA_TEAM_GOODGUYS }
-    Spawners.SpawnConfigs["Radiant_MidA"]  = {Spawner = self.h_RadiantSpawn_Mid_A, FirstWaypoint = self.h_RadiantInit_Mid_Main, Melee = "npc_dota_creep_goodguys_melee", Ranged = "npc_dota_creep_goodguys_ranged", Siege = "npc_dota_goodguys_siege", Team = DOTA_TEAM_GOODGUYS }
-    Spawners.SpawnConfigs["Radiant_MidB"]  = {Spawner = nil,                       FirstWaypoint = self.h_RadiantInit_Mid_AltB, Melee = "npc_dota_creep_goodguys_melee", Ranged = "npc_dota_creep_goodguys_ranged", Siege = "npc_dota_goodguys_siege", Team = DOTA_TEAM_GOODGUYS }
-    Spawners.SpawnConfigs["Radiant_Bot"]   = {Spawner = self.h_RadiantSpawn_Bot,   FirstWaypoint = self.h_RadiantInit_Bot,      Melee = "npc_dota_creep_goodguys_melee", Ranged = "npc_dota_creep_goodguys_ranged", Siege = "npc_dota_goodguys_siege", Team = DOTA_TEAM_GOODGUYS }
+    Spawners.SpawnConfigs[Constants.KEY_RADIANT_TOP]        = {Spawner = self.h_RadiantSpawn_Top,       FirstWaypoint = self.h_RadiantInit_Top,      Melee = "npc_dota_creep_goodguys_melee", Ranged = "npc_dota_creep_goodguys_ranged", Siege = "npc_dota_goodguys_siege", Team = DOTA_TEAM_GOODGUYS }
+    Spawners.SpawnConfigs[Constants.KEY_RADIANT_MIDA]       = {Spawner = self.h_RadiantSpawn_Mid_A,     FirstWaypoint = self.h_RadiantInit_Mid_Main, Melee = "npc_dota_creep_goodguys_melee", Ranged = "npc_dota_creep_goodguys_ranged", Siege = "npc_dota_goodguys_siege", Team = DOTA_TEAM_GOODGUYS }
+    Spawners.SpawnConfigs[Constants.KEY_RADIANT_MIDB]       = {Spawner = nil,                           FirstWaypoint = self.h_RadiantInit_Mid_AltB, Melee = "npc_dota_creep_goodguys_melee", Ranged = "npc_dota_creep_goodguys_ranged", Siege = "npc_dota_goodguys_siege", Team = DOTA_TEAM_GOODGUYS }
+    Spawners.SpawnConfigs[Constants.KEY_RADIANT_BOT]        = {Spawner = self.h_RadiantSpawn_Bot,       FirstWaypoint = self.h_RadiantInit_Bot,      Melee = "npc_dota_creep_goodguys_melee", Ranged = "npc_dota_creep_goodguys_ranged", Siege = "npc_dota_goodguys_siege", Team = DOTA_TEAM_GOODGUYS }
+    Spawners.SpawnConfigs[Constants.KEY_RADIANT_SHELFTOP]   = {Spawner = self.h_RadiantSpawn_ShelfTop,  FirstWaypoint = self.h_RadiantInit_ShelfTop, Melee = "npc_dota_creep_goodguys_melee", Ranged = "npc_dota_creep_goodguys_ranged", Siege = "npc_dota_goodguys_siege", Team = DOTA_TEAM_GOODGUYS }
+    Spawners.SpawnConfigs[Constants.KEY_RADIANT_SHELFBOT]   = {Spawner = self.h_RadiantSpawn_ShelfBot,  FirstWaypoint = self.h_RadiantInit_ShelfBot, Melee = "npc_dota_creep_goodguys_melee", Ranged = "npc_dota_creep_goodguys_ranged", Siege = "npc_dota_goodguys_siege", Team = DOTA_TEAM_GOODGUYS }
 
-    Spawners.SpawnConfigs["Dire_Top"]   = {Spawner = self.h_DireSpawn_Top,   FirstWaypoint = self.h_DireInit_Top,       Melee = "npc_dota_creep_badguys_melee", Ranged = "npc_dota_creep_badguys_ranged", Siege = "npc_dota_badguys_siege", Team = DOTA_TEAM_BADGUYS }
-    Spawners.SpawnConfigs["Dire_MidA"]  = {Spawner = self.h_DireSpawn_Mid_A, FirstWaypoint = self.h_DireInit_Mid_Main,  Melee = "npc_dota_creep_badguys_melee", Ranged = "npc_dota_creep_badguys_ranged", Siege = "npc_dota_badguys_siege", Team = DOTA_TEAM_BADGUYS }
-    Spawners.SpawnConfigs["Dire_MidB"]  = {Spawner = nil,                    FirstWaypoint = self.h_DireInit_Mid_AltB,  Melee = "npc_dota_creep_badguys_melee", Ranged = "npc_dota_creep_badguys_ranged", Siege = "npc_dota_badguys_siege", Team = DOTA_TEAM_BADGUYS }
-    Spawners.SpawnConfigs["Dire_Bot"]   = {Spawner = self.h_DireSpawn_Bot,   FirstWaypoint = self.h_DireInit_Bot,       Melee = "npc_dota_creep_badguys_melee", Ranged = "npc_dota_creep_badguys_ranged", Siege = "npc_dota_badguys_siege", Team = DOTA_TEAM_BADGUYS }
+    Spawners.SpawnConfigs[Constants.KEY_DIRE_TOP]           = {Spawner = self.h_DireSpawn_Top,          FirstWaypoint = self.h_DireInit_Top,        Melee = "npc_dota_creep_badguys_melee",   Ranged = "npc_dota_creep_badguys_ranged",  Siege = "npc_dota_badguys_siege", Team = DOTA_TEAM_BADGUYS }
+    Spawners.SpawnConfigs[Constants.KEY_DIRE_MIDA]          = {Spawner = self.h_DireSpawn_Mid_A,        FirstWaypoint = self.h_DireInit_Mid_Main,   Melee = "npc_dota_creep_badguys_melee",   Ranged = "npc_dota_creep_badguys_ranged",  Siege = "npc_dota_badguys_siege", Team = DOTA_TEAM_BADGUYS }
+    Spawners.SpawnConfigs[Constants.KEY_DIRE_MIDB]          = {Spawner = nil,                           FirstWaypoint = self.h_DireInit_Mid_AltB,   Melee = "npc_dota_creep_badguys_melee",   Ranged = "npc_dota_creep_badguys_ranged",  Siege = "npc_dota_badguys_siege", Team = DOTA_TEAM_BADGUYS }
+    Spawners.SpawnConfigs[Constants.KEY_DIRE_BOT]           = {Spawner = self.h_DireSpawn_Bot,          FirstWaypoint = self.h_DireInit_Bot,        Melee = "npc_dota_creep_badguys_melee",   Ranged = "npc_dota_creep_badguys_ranged",  Siege = "npc_dota_badguys_siege", Team = DOTA_TEAM_BADGUYS }
+    Spawners.SpawnConfigs[Constants.KEY_DIRE_SHELFTOP]      = {Spawner = self.h_DireSpawn_ShelfTop,     FirstWaypoint = self.h_DireInit_ShelfTop,   Melee = "npc_dota_creep_badguys_melee",   Ranged = "npc_dota_creep_badguys_ranged",  Siege = "npc_dota_badguys_siege", Team = DOTA_TEAM_BADGUYS }
+    Spawners.SpawnConfigs[Constants.KEY_DIRE_SHELFBOT]      = {Spawner = self.h_DireSpawn_ShelfBot,     FirstWaypoint = self.h_DireInit_ShelfBot,   Melee = "npc_dota_creep_badguys_melee",   Ranged = "npc_dota_creep_badguys_ranged",  Siege = "npc_dota_badguys_siege", Team = DOTA_TEAM_BADGUYS }
 
     print("Spawners Initialized")
 end
@@ -66,6 +79,9 @@ end
         Min 10+:
         * spawner_good_mid ==> radiant_path_mid_1a ==> radiant_path_mid_right1 => radiant_path_mid_right2 => radiant_path_mid_right3 => radiant_path_mid_recombine ==> radiant_path_end
         * spawner_good_midb => radiant_path_mid_1b ==> radiant_path_mid_left1 ==> radiant_path_mid_left2 ==> radiant_path_mid_left3 ==> radiant_path_mid_recombine ==> radiant_path_end
+        Every 2 min (starting on min 2):
+        * spawner_good_shelf_top ==> radiant_path_shelf_top1 ==> radiant_path_shelf_top2 ==> radiant_path_shelf_top3 ==> radiant_path_end
+        * spawner_good_shelf_bot ==> radiant_path_shelf_bot1 ==> radiant_path_shelf_bot2 ==> radiant_path_shelf_bot3 ==> radiant_path_end
     DIRE
         * spawner_bad_top ==> dire_path_top_1 ==> dire_path_top_2 ==> dire_path_end
         * spawner_bad_bot ==> dire_path_bot_1 ==> dire_path_bot_2 ==> dire_path_end
@@ -74,17 +90,20 @@ end
         Min 10+:
         * spawner_bad_mid ==> dire_path_mid_1a ==> dire_path_mid_right1 => dire_path_mid_right2 => dire_path_mid_right3 => dire_path_mid_recombine ==> dire_path_end
         * spawner_bad_midb => dire_path_mid_1b ==> dire_path_mid_left1 ==> dire_path_mid_left2 ==> dire_path_mid_left3 ==> dire_path_mid_recombine ==> dire_path_end
+        Every 2 min (starting on min 2):
+        * spawner_bad_shelf_top ==> dire_path_shelf_top1 ==> dire_path_shelf_top2 ==> dire_path_shelf_top3 ==> dire_path_end
+        * spawner_bad_shelf_bot ==> dire_path_shelf_bot1 ==> dire_path_shelf_bot2 ==> dire_path_shelf_bot3 ==> dire_path_end
 
     NOTE: Spawner and first waypoint are controlled programmatically, the rest is done via the entities on the map itself
 --]]
 
 function Spawners:DetermineNumMeleeToSpawn(iGameTimeSeconds, sLaneKey)
     local iNum = 0
-    if iGameTimeSeconds < 600 then
+    if iGameTimeSeconds < Constants.TIME_ROUND_2_START then
         iNum = 3
-    elseif iGameTimeSeconds < 1200 then
+    elseif iGameTimeSeconds < Constants.TIME_ROUND_3_START then
         iNum = 4
-    elseif iGameTimeSeconds < 1800 then
+    elseif iGameTimeSeconds < Constants.TIME_ROUND_4_START then
         iNum = 5
     else
         iNum = 6
@@ -92,7 +111,7 @@ function Spawners:DetermineNumMeleeToSpawn(iGameTimeSeconds, sLaneKey)
 
     -- once the lanes start splitting, we don't need to send as many creeps or there will be
     -- way too much XP on the map
-    if (iGameTimeSeconds > self.iSplitTime and string.find(sLaneKey, "Mid")) then 
+    if (iGameTimeSeconds > self.iSplitTime and string.find(sLaneKey, "Mid")) or string.find(sLaneKey, "Shelf") then 
         iNum = iNum - 2
     end
 
@@ -111,6 +130,10 @@ function Spawners:DetermineNumRangedToSpawn(iGameTimeSeconds, sLaneKey)
     -- way too much XP on the map
     if (iGameTimeSeconds > self.iSplitTime and string.find(sLaneKey, "Mid")) then 
         iNum = math.ceil(iNum / 2)
+    end
+
+    if string.find(sLaneKey, "Shelf") then
+        iNum = iNum + 1
     end
 
     return iNum
@@ -137,15 +160,15 @@ end
 -- pit instead of going through it.
 function Spawners:SplitLanes()
     --print("Starting Lane Split")
-    self.SpawnConfigs["Radiant_MidA"].FirstWaypoint = self.h_RadiantInit_Mid_AltA
+    self.SpawnConfigs[Constants.KEY_RADIANT_MIDA].FirstWaypoint = self.h_RadiantInit_Mid_AltA
 
-    self.SpawnConfigs["Radiant_MidB"].Spawner       = self.h_RadiantSpawn_Mid_B
-    self.SpawnConfigs["Radiant_MidB"].FirstWaypoint = self.h_RadiantInit_Mid_AltB
+    self.SpawnConfigs[Constants.KEY_RADIANT_MIDB].Spawner       = self.h_RadiantSpawn_Mid_B
+    self.SpawnConfigs[Constants.KEY_RADIANT_MIDB].FirstWaypoint = self.h_RadiantInit_Mid_AltB
 
-    self.SpawnConfigs["Dire_MidA"].FirstWaypoint    = self.h_DireInit_Mid_AltA
+    self.SpawnConfigs[Constants.KEY_DIRE_MIDA].FirstWaypoint    = self.h_DireInit_Mid_AltA
 
-    self.SpawnConfigs["Dire_MidB"].Spawner          = self.h_DireSpawn_Mid_B
-    self.SpawnConfigs["Dire_MidB"].FirstWaypoint    = self.h_DireInit_Mid_AltB
+    self.SpawnConfigs[Constants.KEY_DIRE_MIDB].Spawner          = self.h_DireSpawn_Mid_B
+    self.SpawnConfigs[Constants.KEY_DIRE_MIDB].FirstWaypoint    = self.h_DireInit_Mid_AltB
 end
 
 -- Loop through the spawn configs and create the creep waves
@@ -156,19 +179,27 @@ function Spawners:SpawnLaneCreeps(iGameTimeSeconds)
         self:SplitLanes()
     end
 
+    -- send shelf wave every 2 min starting on min 2
+    local bSendShelf = false
+    if iGameTimeSeconds > 1 and iGameTimeSeconds % Spawners.iShelfInterval == 0 then
+        bSendShelf = true
+    end
+
     for key,value in pairs(self.SpawnConfigs) do
         if value.Spawner ~= nil then
             --print("Spawning for key " .. key .. " \t\t| At Spawner: " .. value.Spawner:GetName() .. " \t\t| With Goal: " .. value.FirstWaypoint:GetName())
-            -- spawn melee creeps
-            for i = 1, self:DetermineNumMeleeToSpawn(iGameTimeSeconds, key), 1 do
-                local creep = CreateUnitByName( value.Melee, value.Spawner:GetOrigin(), true, nil, nil, value.Team )
-                creep:SetInitialGoalEntity(value.FirstWaypoint)
-            end
+            if (not string.find(key, "Shelf")) or bSendShelf then
+                -- spawn melee creeps
+                for i = 1, self:DetermineNumMeleeToSpawn(iGameTimeSeconds, key), 1 do
+                    local creep = CreateUnitByName( value.Melee, value.Spawner:GetOrigin(), true, nil, nil, value.Team )
+                    creep:SetInitialGoalEntity(value.FirstWaypoint)
+                end
 
-            -- spawn ranged creeps
-            for i = 1, self:DetermineNumRangedToSpawn(iGameTimeSeconds, key), 1 do
-                local creep = CreateUnitByName( value.Ranged, value.Spawner:GetOrigin(), true, nil, nil, value.Team )
-                creep:SetInitialGoalEntity(value.FirstWaypoint)
+                -- spawn ranged creeps
+                for i = 1, self:DetermineNumRangedToSpawn(iGameTimeSeconds, key), 1 do
+                    local creep = CreateUnitByName( value.Ranged, value.Spawner:GetOrigin(), true, nil, nil, value.Team )
+                    creep:SetInitialGoalEntity(value.FirstWaypoint)
+                end
             end
         end
     end
