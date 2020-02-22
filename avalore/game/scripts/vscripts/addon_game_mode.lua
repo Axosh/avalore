4b6af724e8f8f2c7d59807e24ecaff822034e642
Axosh <axosh.games@gmail.com>
2020-02-22 03:45:36 -0600

    After many attempts to create an interaction similar to how outpost capturing works, falling back onto using the actual outposts for now. Set up a basic system to disable them until a certain time, then allow for tracking how many seconds each team has captured them for. Mostly checking this in as a debug state at the moment, will clean up later.

--- avalore/game/scripts/vscripts/addon_game_mode.lua
+++ avalore/game/scripts/vscripts/addon_game_mode.lua
@@ -8,6 +8,15 @@ _G.round = 0
 _G.GoodScore = 0
 _G.BadScore = 0
 
+radi_outpost = nil
+t_radi_outpost = {}
+t_radi_outpost.radiTime = 0
+t_radi_outpost.direTime = 0
+dire_outpost = nil
+t_dire_outpost = {}
+t_dire_outpost.radiTime = 0
+t_dire_outpost.direTime = 0
+
 ---------------------------------------------------------------------------
 -- CAvaloreGameMode class
 ---------------------------------------------------------------------------
@@ -32,6 +41,9 @@ function Precache( context )
 			PrecacheResource( "particle", "*.vpcf", context )
 			PrecacheResource( "particle_folder", "particles/folder", context )
 	]]
+
+	LinkLuaModifier( "modifier_unselectable", "scripts/vscripts/round2/building_outpost.lua", LUA_MODIFIER_MOTION_NONE )
+	LinkLuaModifier( "modifier_capturable", "scripts/vscripts/round2/building_outpost.lua", LUA_MODIFIER_MOTION_NONE )
 end
 
 -- Create the game mode when we activate
@@ -64,10 +76,24 @@ function CAvaloreGameMode:InitGameMode()
 		dire_score = 0
 	}
 	CustomGameEventManager:Send_ServerToAllClients( "refresh_score", score_obj )
+
+
+	--local outpostTest = CreateUnitByName( 'radiant_outpost', Vector(-3904, 3008, 384), true, nil, nil, DOTA_TEAM_NOTEAM )--Entities:FindByName(nil, "radiant_outpost")
+	--outpostTest:AddNewModifier(outpostTest, nil, "modifier_unselectable", {})
+	--temp_outpost = CreateUnitByName( 'radiant_outpost', Vector(-3904, 3008, 394), true, nil, nil, DOTA_TEAM_NOTEAM )
+	--temp_outpost:AddNewModifier(outpostTest, nil, "modifier_unselectable", {})
+
+	radi_outpost = Entities:FindByName(nil, "radiant_outpost")
+	radi_outpost:SetTeam(DOTA_TEAM_NEUTRALS)
+	radi_outpost:AddNewModifier(outpostTest, nil, "modifier_unselectable", {})
+	dire_outpost = Entities:FindByName(nil, "dire_outpost")
+	dire_outpost:SetTeam(DOTA_TEAM_NEUTRALS)
+	dire_outpost:AddNewModifier(outpostTest, nil, "modifier_unselectable", {})
 end
 
 -- Evaluate the state of the game
 function CAvaloreGameMode:OnThink()
+
 	--grab current time as a float, excluding pregame and negative time
 	curr_gametime = GameRules:GetDOTATime(false, false)--GameRules:GetGameTime()
 	if self.countdownEnabled == true then
@@ -105,6 +131,13 @@ function CAvaloreGameMode:OnThink()
 	elseif curr_gametime > 0 then
 		--print("capture")
 		if(_G.round < 1) then
+			--TEMP ==> force debug: set hero to lvl 6
+			local p1_hero = PlayerResource:GetSelectedHeroEntity(0)
+			p1_hero:HeroLevelUp(false)
+			p1_hero:HeroLevelUp(false)
+			p1_hero:HeroLevelUp(false)
+			p1_hero:HeroLevelUp(false)
+			p1_hero:HeroLevelUp(false)
 			_G.round = 1
 			for i = 0,6,1
 			do
@@ -126,7 +159,54 @@ function CAvaloreGameMode:OnThink()
 			}
 			CustomGameEventManager:Send_ServerToAllClients( "broadcast_message", broadcast_obj )
 		end
+
+		-- temp test
+		if(math.floor(curr_gametime) == 5) then
+			print("clearing modifier")
+			dire_outpost:RemoveModifierByName("modifier_unselectable")
+			radi_outpost:RemoveModifierByName("modifier_unselectable")
+			--local outpostTest = Entities:FindByName(nil, "radiant_outpost")
+			--outpostTest:RemoveModifierByName("modifier_unselectable")
+			--temp_outpost:RemoveModifierByName("modifier_unselectable")
+			--temp_outpost:RemoveModifierByName("modifier_invulnerable")
+			--temp_outpost:AddNewModifier(outpostTest, nil, "modifier_capturable", {})
+			--local TempTest = Entities:FindByName(nil, "radiant_outpost")
+			--TempTest:RemoveModifierByName("modifier_invulnerable")
+			--TempTest:AddNewModifier(outpostTest, nil, "modifier_capturable", {})
+
+			--[[print("temp_outpost")
+			for key, value in pairs(temp_outpost:FindAllModifiers()) do
+				print(value:GetName())
+			end
+			print("TempTest")
+			for key, value in pairs(TempTest:FindAllModifiers()) do
+				print(value:GetName())
+			end
+			--]]
+			
+		end
+	elseif curr_gametime == 0 then
+		dire_outpost:SetTeam(DOTA_TEAM_NOTEAM)
+		radi_outpost:SetTeam(DOTA_TEAM_NOTEAM)
 	end
+
+	if radi_outpost:GetTeam() == DOTA_TEAM_GOODGUYS then
+		t_radi_outpost.radiTime = t_radi_outpost.radiTime + 1
+	elseif radi_outpost:GetTeam() == DOTA_TEAM_BADGUYS then
+		t_radi_outpost.direTime = t_radi_outpost.direTime + 1
+	end
+
+	if dire_outpost:GetTeam() == DOTA_TEAM_GOODGUYS then
+		t_dire_outpost.radiTime = t_dire_outpost.radiTime + 1
+	elseif dire_outpost:GetTeam() == DOTA_TEAM_BADGUYS then 
+		t_dire_outpost.direTime = t_dire_outpost.direTime + 1
+	end
+
+	print("R-Outpost RTime = " .. tostring(t_radi_outpost.radiTime))
+	print("R-Outpost DTime = " .. tostring(t_radi_outpost.direTime))
+	print("D-Outpost RTime = " .. tostring(t_dire_outpost.radiTime))
+	print("D-Outpost DTime = " .. tostring(t_dire_outpost.direTime))
+	print("******************************")
 
 	--TEMP
 	--print("GameTime = " .. tostring(curr_gametime) .. ", Eval = " .. tostring(math.floor(curr_gametime) % 30))
