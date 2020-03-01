06d5bb76c8ae9c018d10504a097c5a97dbc697b0
Axosh <axosh.games@gmail.com>
2020-02-29 18:45:15 -0600

    got a basic flag capture mechanic working when you run over a flag base

--- avalore/game/scripts/vscripts/debug.lua
+++ avalore/game/scripts/vscripts/debug.lua
@@ -2,7 +2,7 @@
     Utility functions for helping debug.
 --]]
 
-function DebugVector( vector )
-    local result = "(" .. tostring(vector.X) .. ", " .. tostring(vector.Y) .. ", " .. tostring(vector.Z) .. ")"
-    return result
-end
+-- function DebugVector( vector )
+--     local result = "(" .. tostring(vector.X) .. ", " .. tostring(vector.Y) .. ", " .. tostring(vector.Z) .. ")"
+--     return result
+-- end
