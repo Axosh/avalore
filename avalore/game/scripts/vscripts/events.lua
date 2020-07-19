c9482631d0c6ad53c49b81a4c408e65b94e88a59
Axosh <axosh.games@gmail.com>
2020-07-18 23:51:44 -0500

    Score calculation updates
    - fixed a lot of score calculation on unit killed
    - updated constants
    - player score tracking
    - game end logic

--- avalore/game/scripts/vscripts/events.lua
+++ avalore/game/scripts/vscripts/events.lua
@@ -16,16 +16,38 @@ function CAvaloreGameMode:OnEntityKilled(event)
 	local attackerEntity 	= EntIndexToHScript( event.entindex_attacker )
 	local attackerTeam 		= nil --attackerEntity:GetTeam()
 	local curr_gametime 	= GameRules:GetDOTATime(false, false)
+	local isPlayer 			= attackerEntity:GetPlayerOwnerID() > -1 -- TODO: Test for summons/illu
+	local isDeny			= false
+	local teamString 		= ""
+	local winnning_team 	= nil -- setting our own variable to update the scores one last time before game ends
+
+	if killedTeam == DOTA_TEAM_BADGUYS then
+		teamString = "dire"
+	elseif killedTeam == DOTA_TEAM_GOODGUYS then
+		teamString = "radi"
+	end
 
 	local refreshScores = false
 
 	if attackerEntity ~= nil then
 		attackerTeam = attackerEntity:GetTeam()
+		if attackerTeam == killedTeam then
+			isDeny = true
+		end
+	end
+
+	if attackerEntity:IsRealHero() then
+		print("Killed Entity: " .. killedEntity:GetUnitName())
 	end
 
 	--Check for bonus points due to quest objective
 	local objectivePoints = 0
-	if killedEntity:GetUnitName() == "npc_avalore_quest_wisp" then
+
+	-- ==========================
+	-- ROUND 1 OBJECTIVES
+	-- ==========================
+
+	if killedEntity:GetUnitName() == ROUND1_WISP_UNIT then
 		--objectivePoints = 3
 		refreshScores = true
 		local first_wisp = false
@@ -56,6 +78,10 @@ function CAvaloreGameMode:OnEntityKilled(event)
 			end -- end for-loop
 		end
 	end
+
+	-- ==========================
+	-- ROUND 3 OBJECTIVES
+	-- ==========================
 
 	-- check for gem drop in round 3
 	if curr_gametime > Constants.TIME_ROUND_3_START and curr_gametime < Constants.TIME_ROUND_4_START then
@@ -89,7 +115,7 @@ function CAvaloreGameMode:OnEntityKilled(event)
 			end
 		end
 	end
-	if killedEntity:GetUnitName() == "npc_avalore_gem_boss" then
+	if killedEntity:GetUnitName() == ROUND3_BOSS_UNIT then
 		refreshScores = true
 		if attackerTeam == DOTA_TEAM_GOODGUYS then
 			Score.round3.radi_boss_kills = Score.round3.radi_boss_kills + 1
@@ -98,15 +124,90 @@ function CAvaloreGameMode:OnEntityKilled(event)
 		end
 	end
 
+	-- ==========================
+	-- HERO KILLS
+	-- ==========================
+
 	--Hero Kills, excluding denies
 	if killedEntity:IsRealHero() and attackerTeam ~= killedTeam then
 		--objectivePoints = 1
 		refreshScores = true
 	end
 
+	--============================
+	-- BUILDING KILLS
+	--============================
+
+	-- ***** TOWER ****
+	if string.find(string.lower(killedEntity:GetUnitName()), "tower") then
+		refreshScores = true
+		print("Tower Killed: " .. killedEntity:GetUnitName())
+		if string.find(killedEntity:GetUnitName(), "1") then
+			Score.towers[teamString][BuildingLaneLocation(killedEntity:GetUnitName()) .. "1"] = false
+			if isPlayer and not isDeny then
+				Score.playerStats[attackerEntity:GetPlayerOwnerID()].t1 = Score.playerStats[attackerEntity:GetPlayerOwnerID()].t1 + 1
+			end
+		elseif string.find(killedEntity:GetUnitName(), "2") then
+			Score.towers[teamString][BuildingLaneLocation(killedEntity:GetUnitName()) .. "2"] = false
+			if isPlayer and not isDeny then
+				Score.playerStats[attackerEntity:GetPlayerOwnerID()].t2 = Score.playerStats[attackerEntity:GetPlayerOwnerID()].t2 + 1
+			end
+		elseif string.find(killedEntity:GetUnitName(), "3") then
+			Score.towers[teamString][BuildingLaneLocation(killedEntity:GetUnitName()) .. "3"] = false
+			if isPlayer and not isDeny then
+				Score.playerStats[attackerEntity:GetPlayerOwnerID()].t3 = Score.playerStats[attackerEntity:GetPlayerOwnerID()].t3 + 1
+			end
+		elseif string.find(killedEntity:GetUnitName(), "4") then
+			Score.towers[teamString]["t4" .. BuildingLaneLocation(killedEntity:GetUnitName())] = false
+			if isPlayer and not isDeny then
+				Score.playerStats[attackerEntity:GetPlayerOwnerID()].t4 = Score.playerStats[attackerEntity:GetPlayerOwnerID()].t4 + 1
+			end
+		end
+	end
+
+	-- ***** RAX *****
+	if string.find(string.lower(killedEntity:GetUnitName()), "rax") then
+		refreshScores = true
+		print("Rax Killed: " .. killedEntity:GetUnitName())
+		if string.find(killedEntity:GetUnitName(), "melee") then
+			Score.raxes[teamString][BuildingLaneLocation(killedEntity:GetUnitName()) .. "melee"] = false
+			if isPlayer and not isDeny then
+				Score.playerStats[attackerEntity:GetPlayerOwnerID()].meleeRax = Score.playerStats[attackerEntity:GetPlayerOwnerID()].meleeRax + 1
+			end
+		elseif string.find(killedEntity:GetUnitName(), "ranged") then
+			Score.raxes[teamString][BuildingLaneLocation(killedEntity:GetUnitName()) .. "ranged"] = false
+			if isPlayer and not isDeny then
+				Score.playerStats[attackerEntity:GetPlayerOwnerID()].rangeRax = Score.playerStats[attackerEntity:GetPlayerOwnerID()].rangeRax + 1
+			end
+		end
+	end
+	--end
+
+	-- ***** ANCIENT *****
+	if killedEntity:GetUnitName() == OBJECTIVE_DIRE_BASE then
+		winnning_team = DOTA_TEAM_GOODGUYS
+		refreshScores = true
+	elseif killedEntity:GetUnitName() == OBJECTIVE_RADI_BASE then
+		winnning_team = DOTA_TEAM_BADGUYS
+		refreshScores = true
+	end
+
 	-- only update front-end if score changed
 	if refreshScores then
+		if winnning_team ~= nil then
+			print("Winning Team = " .. tostring(winnning_team))
+			if isPlayer and not isDeny then
+				-- this should only ever be 0 or 1
+				Score.playerStats[attackerEntity:GetPlayerOwnerID()].base_kill = 1
+			end
+		end
+
 		Score:RecalculateScores()
+
+		-- this will immediately end the game
+		if winnning_team ~= nil then
+			GAME_WINNER_TEAM = winnning_team
+		end
 	-- if objectivePoints > 0 then 
 	-- 	if attackerTeam == DOTA_TEAM_GOODGUYS then
 	-- 		Score.RadiScore = Score.RadiScore + objectivePoints
@@ -140,6 +241,21 @@ function CAvaloreGameMode:OnEntityKilled(event)
 	end
 
 	--print("OnEntityKilled - Ended")
+end
+
+function BuildingLaneLocation(building_unit_name)
+	local result = ""
+	
+	-- check top/bot first since there are more of those (tier4s)
+	if string.find(building_unit_name, "top") then
+		result = "top"
+	elseif string.find(building_unit_name, "bot") then
+		result = "bot"
+	elseif string.find(building_unit_name, "mid") then
+		result = "mid"
+	end
+
+	return result
 end
 
 function IsAncientCreep(creep_name)
