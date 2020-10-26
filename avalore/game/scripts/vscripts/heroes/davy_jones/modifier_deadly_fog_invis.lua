650a7274428b31b6d14c1b197f26087a611c9511
Axosh <axosh.games@gmail.com>
2020-10-25 22:11:05 -0500

    GitIgnore Update + Cleaned up first ability + Made second ability
    - added more stuff to gitignore
    - fixed deadly fog so it is basically working now other than balance
    - started working on whirlpool => it works, but doesn't always suck units in correctly or release them correctly either
    - added libraries folder to house stuff that extends some of the base classes
    - refactored some ability logic into a common shared folder

--- avalore/game/scripts/vscripts/heroes/davy_jones/modifier_deadly_fog_invis.lua
+++ avalore/game/scripts/vscripts/heroes/davy_jones/modifier_deadly_fog_invis.lua
@@ -1,23 +1,85 @@
 modifier_deadly_fog_invis = class({})
 
+function modifier_deadly_fog_invis:GetAttributes() return MODIFIER_ATTRIBUTE_MULTIPLE end
+
 function modifier_deadly_fog_invis:IsHidden() return false end
 function modifier_deadly_fog_invis:IsPurgable() return false end
 function modifier_deadly_fog_invis:IsDebuff() return false end
 
 function modifier_deadly_fog_invis:DeclareFunctions()
-    return {MODIFIER_PROPERTY_INVISIBILITY_LEVEL}
+    return {MODIFIER_PROPERTY_INVISIBILITY_LEVEL,
+            MODIFIER_EVENT_ON_ATTACK,
+            MODIFIER_EVENT_ON_ABILITY_FULLY_CAST}
 end
 
 function modifier_deadly_fog_invis:GetModifierInvisibilityLevel()
-    return 1
+    if self:GetStackCount() == 0 then
+        return 1
+    else
+        return 0
+    end
 end
 
 function modifier_deadly_fog_invis:CheckState()
-    return  {   [MODIFIER_STATE_NO_UNIT_COLLISION] = true,
-                [MODIFIER_STATE_INVISIBLE] = true
-            }
+    if IsServer() then
+        if self:GetStackCount() == 0 then
+            return  {   [MODIFIER_STATE_NO_UNIT_COLLISION] = true,
+                        [MODIFIER_STATE_INVISIBLE] = true
+                    }
+        else
+            return  {   [MODIFIER_STATE_NO_UNIT_COLLISION] = false,
+                        [MODIFIER_STATE_INVISIBLE] = false
+                    }
+        end
+    end
 end
 
 function modifier_deadly_fog_invis:GetPriority()
     return MODIFIER_PRIORITY_NORMAL
+end
+
+function modifier_deadly_fog_invis:OnCreated()
+    self.caster = self:GetCaster()
+	self.ability = self:GetAbility()
+	self.parent = self:GetParent()
+
+	local particle = ParticleManager:CreateParticle("particles/generic_hero_status/status_invisibility_start.vpcf", PATTACH_ABSORIGIN, self:GetParent())
+    ParticleManager:ReleaseParticleIndex(particle)
+    self:SetStackCount(0) -- start out invis when trigger the spell
+    self:StartIntervalThink( 1 )
+end
+
+function modifier_deadly_fog_invis:OnAttack(keys)
+    if IsServer() then
+        local attacker = keys.attacker
+        -- Only apply if the parent is the one attacking
+        if self.parent == attacker then
+            --print("[modifier_deadly_fog_invis] OnAttack")
+            self:SetStackCount(3)
+        end
+    end
+end
+
+function modifier_deadly_fog_invis:OnAbilityFullyCast(keys)
+    if IsServer() then
+        local caster = keys.unit
+        -- Only apply if the parent is the one attacking
+        if self.parent == caster then
+            --print("[modifier_deadly_fog_invis] OnAttack")
+            self:SetStackCount(3)
+        end
+    end
+end
+
+function modifier_deadly_fog_invis:OnIntervalThink()
+    if IsServer() then
+        local stacks = self:GetStackCount()
+        if stacks > 0 then
+            self:SetStackCount(stacks - 1)
+            if (stacks - 1 ) == 0 then
+                local particle = ParticleManager:CreateParticle("particles/generic_hero_status/status_invisibility_start.vpcf", PATTACH_ABSORIGIN, self:GetParent())
+                ParticleManager:ReleaseParticleIndex(particle)
+            end
+        end
+    end
 end
