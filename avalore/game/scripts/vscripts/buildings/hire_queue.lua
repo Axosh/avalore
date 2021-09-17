c563ac80643c6ddea773cd5884406f1afc919887
Axosh <axosh.games@gmail.com>
2021-09-17 12:43:34 -0500

    Mercs + Creeps - cleanup and fleshing out
    - split units up into their own files
    - implemented 3 different merc units
    - languaged troops
    - built out hire functions with shared queue function

--- avalore/game/scripts/vscripts/buildings/hire_queue.lua
+++ avalore/game/scripts/vscripts/buildings/hire_queue.lua
@@ -3,43 +3,15 @@ require("spawners")
 require("utility_functions")
 
 hire_queue = class({})
-avalore_merc_train_melee_creep = class({})
 
--- function HireQueue:Init(building)
---     print("HireQueue:Init()")
--- end
 
--- -- Static function
--- function Enqueue(event)
---     print("Enqueue()")
---     local caster = event.caster
---     local ability = event.ability
---     local playerID = caster:GetPlayerOwnerID()
---     local gold_cost = ability:GetGoldCost( ability:GetLevel() - 1 )
+-- ==================================================================
+-- SHARED FUNCTIONS
+-- ==================================================================
 
---     if not self.queue then
---         self.queue = {}
---     end
+function HireUnit(unit, team, gold_cost, merc_camp_building)
 
---     -- TODO: don't hard-code this
---     if not Score.RadiSharedGoldCurr >= gold_cost then
---         SendErrorMessage(playerID, "#error_not_enough_gold_radiant")
---     end
-
---     -- TODO: limit how many units can be hired
--- end
-
-function avalore_merc_train_melee_creep:OnSpellStart()
-    if not IsServer() then return end
-    print("HireQueue:OnSpellStart()")
-    local caster = self:GetCaster()
-    -- if caster then
-    --     print(caster:GetPlayer())
-    -- end
-    local team = caster:GetTeam()
-    local gold_cost = self:GetSpecialValueFor("gold_cost")
-    local unit = ""
-
+    -- check that they have enough gold and append correct unit to spawn
     if team == DOTA_TEAM_GOODGUYS then
         if not (Score.RadiSharedGoldCurr > gold_cost) then
             print("[HireQueue:OnSpellStart()] Not enough gold")
@@ -53,72 +25,116 @@ function avalore_merc_train_melee_creep:OnSpellStart()
 			-- CustomGameEventManager:Send_ServerToPlayer(PlayerResource:GetPlayer(caster:GetPlayerID()), "broadcast_message", broadcast_obj )
             return
         end
-        unit = "npc_dota_creep_goodguys_melee" --self:GetSpecialValueFor("unit_radi")
+        unit = unit .. "radi"
     else
         if not (Score.DireSharedGoldCurr > gold_cost) then
             print("[HireQueue:OnSpellStart()] Not enough gold")
-            local broadcast_obj = 
-			{
-				msg = "#error_not_enough_gold_radiant",
-				time = 5,
-				elaboration = "",
-				type = MSG_TYPE_ERROR
-			}
-			CustomGameEventManager:Send_ServerToPlayer(PlayerResource:GetPlayer(caster:GetPlayerID()), "broadcast_message", broadcast_obj )
+            -- local broadcast_obj = 
+			-- {
+			-- 	msg = "#error_not_enough_gold_radiant",
+			-- 	time = 5,
+			-- 	elaboration = "",
+			-- 	type = MSG_TYPE_ERROR
+			-- }
+			-- CustomGameEventManager:Send_ServerToPlayer(PlayerResource:GetPlayer(caster:GetPlayerID()), "broadcast_message", broadcast_obj )
             return
         end
-        unit = "npc_dota_creep_badguys_melee" -- self:GetSpecialValueFor("unit_dire")
+        unit = unit .. "dire"
     end
-    
 
-    --local lane = self:GetOwner().lane
     local lane = ""
-    print("==== FIND LANE OF QUEUE ====")
-    print("Owner: " .. tostring(self:GetOwner()))
-    for key, value in pairs(Spawners.MercCamps[self:GetOwner():GetTeam()]) do
+    --print("==== FIND LANE OF QUEUE ====")
+    --print("Owner: " .. tostring(self:GetOwner()))
+    for key, value in pairs(Spawners.MercCamps[team]) do
         print("Value: " .. tostring(value))
-        if self:GetOwner() == value then
+        if merc_camp_building == value then
             lane = key
         end
     end
-    -- if self:GetOwner():GetAbsOrigin().x < 0 then
-    --     if self:GetOwner():GetTeam() == DOTA_TEAM_BADGUYS then
-    --         lane = Constants.KEY_DIRE_TOP
-    --     else
-    --         lane = Constants.KEY_RADIANT_TOP
-    --     end
-    -- else
-    --     if self:GetOwner():GetTeam() == DOTA_TEAM_BADGUYS then
-    --         lane = Constants.KEY_DIRE_BOT
-    --     else
-    --         lane = Constants.KEY_RADIANT_BOT
-    --     end
-    -- end
 
-    -- if not Spawners.MercQueue[lane] then
-    --     Spawners.MercQueue[lane] = {}
-    -- end
-
-    --local unit_to_queue = self:MapUnit()
     print("Queueing up a " .. unit .. " in lane " .. lane)
     table.insert(Spawners.MercQueue[team][lane], unit)
 
     PrintTable(Spawners.MercQueue[team][lane])
 end
 
+-- ==================================================================
+-- TRAIN MELEE
+-- ==================================================================
+
+avalore_merc_train_melee_creep = class({})
+
+function avalore_merc_train_melee_creep:OnSpellStart()
+    if not IsServer() then return end
+    print("HireQueue:OnSpellStart()")
+    local caster = self:GetCaster()
+    -- if caster then
+    --     print(caster:GetPlayer())
+    -- end
+    local team = caster:GetTeam()
+    local gold_cost = self:GetSpecialValueFor("gold_cost")
+    local unit = "npc_avalore_merc_melee_"
+
+   HireUnit(unit, team, gold_cost, self:GetOwner())
+end
+
+-- ==================================================================
+-- TRAIN RANGED
+-- ==================================================================
+
+avalore_merc_train_ranged_creep = class({})
+
+function avalore_merc_train_ranged_creep:OnSpellStart()
+    if not IsServer() then return end
+    print("HireQueue:OnSpellStart()")
+    local caster = self:GetCaster()
+    -- if caster then
+    --     print(caster:GetPlayer())
+    -- end
+    local team = caster:GetTeam()
+    local gold_cost = self:GetSpecialValueFor("gold_cost")
+    local unit = "npc_avalore_merc_ranged_"
+
+   HireUnit(unit, team, gold_cost, self:GetOwner())
+end
+
+-- ==================================================================
+-- TRAIN HULK
+-- ==================================================================
+
+avalore_merc_train_hulk_creep = class({})
+
+function avalore_merc_train_hulk_creep:OnSpellStart()
+    if not IsServer() then return end
+    print("HireQueue:OnSpellStart()")
+    local caster = self:GetCaster()
+    -- if caster then
+    --     print(caster:GetPlayer())
+    -- end
+    local team = caster:GetTeam()
+    local gold_cost = self:GetSpecialValueFor("gold_cost")
+    local unit = "npc_avalore_merc_hulk_"
+
+   HireUnit(unit, team, gold_cost, self:GetOwner())
+end
+
+-- ==================================================================
+-- OTHER
+-- ==================================================================
+
 -- function DequeueUnit( event )
 --     -- TODO
 -- end
 
-function avalore_merc_train_melee_creep:MapUnit(unit, team)
-    local prefix = ""
-    if team == DOTA_TEAM_GOODGUYS then
-        prefix = "npc_dota_creep_goodguys_"
-    else
-        prefix = "npc_dota_creep_badguys_"
-    end
+-- function avalore_merc_train_melee_creep:MapUnit(unit, team)
+--     local prefix = ""
+--     if team == DOTA_TEAM_GOODGUYS then
+--         prefix = "npc_dota_creep_goodguys_"
+--     else
+--         prefix = "npc_dota_creep_badguys_"
+--     end
 
-    if unit == "melee" or unit == "ranged" then
-        return (prefix .. unit)
-    end
-end
+--     if unit == "melee" or unit == "ranged" then
+--         return (prefix .. unit)
+--     end
+-- end
