3dad4d0dbd65487bb98e6824c84965312bbb46a6
Axosh <axosh.games@gmail.com>
2021-09-24 23:49:21 -0500

    Dionysus - Drunk Modifier

--- avalore/game/scripts/vscripts/heroes/dionysus/modifier_drunk.lua
+++ avalore/game/scripts/vscripts/heroes/dionysus/modifier_drunk.lua
@@ -15,7 +15,7 @@ function modifier_drunk:StatusEffectPriority()
 	return MODIFIER_PRIORITY_NORMAL
 end
 
-function modifier_imba_brewmaster_cinder_brew:DeclareFunctions()
+function modifier_drunk:DeclareFunctions()
 	return {
         --MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE,
         MODIFIER_PROPERTY_MISS_PERCENTAGE,
@@ -31,6 +31,8 @@ function modifier_drunk:OnCreated()
     self.REDUCED = 2 -- 50% damage
     
     self.miss_chance = 0 -- changes based on whether next attack is a miss
+    self.percent_dmg = 100
+    self.reduction = self:GetAbility():GetSpecialValueFor("reduction")
 
     self.attack_queue_template = {self.HIT, self.MISS, self.REDUCED}
     self:ShuffleAttackQueue()
@@ -47,9 +49,33 @@ function modifier_drunk:OnAttackStart(keys)
         local next = next --https://stackoverflow.com/questions/1252539/most-efficient-way-to-determine-if-a-lua-table-is-empty-contains-no-entries
         local next_attack = table.remove(self.attack_queue)
 
+        if next_attack == self.MISS then
+            self.miss_chance = 1
+        elseif next_attack == self.REDUCED then
+            self.percent_dmg = 100 - self.reduction
+        end
+
         if not next(self.attack_queue) then
             self:ShuffleAttackQueue()
         end
     end
 end
 
+function modifier_drunk:GetModifierMiss_Percentage()
+    if self.miss_rate >  0 then
+        local result = self.miss_rate
+        self.miss_rate = 0
+        return result
+    end
+    return 0
+end
+
+function modifier_drunk:GetModifierTotalDamageOutgoing_Percentage(keys)
+    if self.percent_dmg < 100 then
+        self:GetParent():EmitSound("Hero_BrewMaster.CinderBrew.SelfAttack")
+        local result = self.percent_dmg
+        self.percent_dmg = 100
+        return result
+    end
+    return 100
+end
