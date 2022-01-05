f3198359ccb12674b9a8262f48970951a9e19d1c
Axosh <axosh.games@gmail.com>
2022-01-04 21:16:46 -0600

    Merc System - Demi Heroes [WIP]
    some baby steps in the framework

---
+++ avalore/game/scripts/vscripts/controllers/demi_hero_manager.lua
@@ -0,0 +1,25 @@
+require("constants")
+require("references")
+require(REQ_UTIL)
+
+if DemiHeroManager == nil then
+    DemiHeroManager = {}
+end
+
+function DemiHeroManager:AddTeam(team)
+    DemiHeroManager[team] = {}
+end
+
+-- if unit exists, check to see what level it spawned as last, add 1 to it, and return the level
+-- otherwise, insert it and set level to 1
+function DemiHeroManager:HireDemiHero(team, unit)
+    local demi_hero_level = DemiHeroManager[team][unit:GetName()]
+    if demi_hero_level then
+        demi_hero_level = demi_hero_level + 1
+        DemiHeroManager[team][unit:GetName()] = demi_hero_level
+    else
+        DemiHeroManager[team][unit:GetName()] = 1
+    end
+
+    return DemiHeroManager[team][unit:GetName()]
+end
