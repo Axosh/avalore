1296cad2b968bbad0633148755367253dc26e67f
Axosh <axosh.games@gmail.com>
2022-01-14 18:28:20 -0600

    Thor - Hammer Toss [WIP]

--- avalore/game/scripts/vscripts/heroes/thor/ability_hammer_toss.lua
+++ avalore/game/scripts/vscripts/heroes/thor/ability_hammer_toss.lua
@@ -11,10 +11,82 @@ function ability_hammer_toss:Precache( context )
 	PrecacheResource( "particle", "particles/units/heroes/hero_dawnbreaker/dawnbreaker_converge_debuff.vpcf", context )
 end
 
+--------------------------------------------------------------------------------
+-- Ability Cast Filter
+function ability_hammer_toss:CastFilterResultLocation( vLoc )
+	-- check nohammer
+	if self:GetCaster():HasModifier( "modifier_no_hammer" ) then
+		return UF_FAIL_CUSTOM
+	end
+
+	return UF_SUCCESS
+end
+
+function ability_hammer_toss:GetCustomCastErrorLocation( vLoc )
+	-- check nohammer
+	if self:GetCaster():HasModifier( "modifier_no_hammer" ) then
+		return "#dota_hud_error_nohammer"
+	end
+
+	return ""
+end
+
 function ability_hammer_toss:OnSpellStart()
 	local caster 	= self:GetCaster()
 	local target 	= self:GetVectorTargetPosition()
 	local radius 	= self:GetSpecialValueFor( "projectile_radius" )
 	local speed 	= self:GetSpecialValueFor( "projectile_speed" )
 	local distance 	= self:GetSpecialValueFor( "range" )
+
+	local direction = target.direction:Normalized()
+
+	-- create thinker
+	local thinker = CreateModifierThinker(
+		caster, -- player source
+		self, -- ability source
+		"modifier_hammer_toss_thinker", -- modifier name
+		{}, -- kv
+		caster:GetOrigin(),
+		self:GetCaster():GetTeamNumber(),
+		false
+	)
+
+	-- create projectile
+	local info = {
+		Source = caster,
+		Ability = self,
+		vSpawnOrigin = caster:GetAbsOrigin(),
+	
+		-- bDeleteOnHit = true,
+		iUnitTargetTeam = DOTA_UNIT_TARGET_TEAM_ENEMY,
+		iUnitTargetFlags = DOTA_UNIT_TARGET_FLAG_NONE,
+		iUnitTargetType = DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
+	
+		EffectName = name,
+		fDistance = distance,
+		fStartRadius = radius,
+		fEndRadius = radius,
+		vVelocity = direction * speed,
+	}
+	local data = {
+		cast = 1,
+		targets = {},
+		thinker = thinker,
+	}
+
+	local id = ProjectileManager:CreateLinearProjectile( info )
+	thinker.id = id
+	self.projectiles[id] = data
+	table.insert( self.thinkers, thinker )
+
+	-- set no hammer
+	caster:AddNewModifier(
+		caster, -- player source
+		self, -- ability source
+		"modifier_no_hammer", -- modifier name
+		{} -- kv
+	)
+
+	-- play effects
+	data.effect = self:PlayEffects1( caster:GetOrigin(), distance, direction * speed )
 end
