6b271852a2d5fe43fcf71fa4abd978aa9c822500
Axosh <axosh.games@gmail.com>
2022-01-24 23:18:27 -0600

    Thor - Thunder Clap [WIP]
    - more work on the pathing block part of the spell

--- avalore/game/scripts/vscripts/heroes/thor/ability_thunder_clap.lua
+++ avalore/game/scripts/vscripts/heroes/thor/ability_thunder_clap.lua
@@ -15,34 +15,7 @@ function ability_thunder_clap:OnSpellStart()
 	local radius        = self:GetSpecialValueFor("radius")
 	--local slow_duration = self:GetSpecialValueFor("slow_duration")
 
-    local block_width = 24
-	local block_delta = 8.25
-
-    -- Create blocker along path
-	--local block_spacing = (block_delta+2*block_width)
-	local blocks = 8 --distance/block_spacing
-	--local block_pos = caster:GetHullRadius() + block_delta + block_width
-	--local start_pos = caster:GetOrigin() + direction*block_pos
-
-	for i=1,blocks do
-        local progress = i/blocks
-        local theta = -2 * math.pi * progress
-		local x =  math.sin( theta ) * block_width * 0.5
-		local y = -math.cos( theta ) * block_delta * 0.5
-        local block_vec = Vector(x, y, caster:GetAbsOrigin().z)
-		--local block_vec = caster:GetOrigin() + direction*block_pos
-		local blocker = CreateModifierThinker(
-			caster, -- player source
-			self, -- ability source
-			"modifier_erect_wall_thinker", -- modifier name
-			{ duration = duration }, -- kv
-			block_vec,
-			caster:GetTeamNumber(),
-			true
-		)
-		blocker:SetHullRadius( block_width )
-		--block_pos = block_pos + block_spacing
-	end
+    self:MakeBlockers(caster)
 
     -- -- get list of affected enemies by the slow/damage
 	-- local enemies = FindUnitsInRadius (
@@ -121,4 +94,62 @@ function ability_thunder_clap:PlayEffects()
 
 	-- play sounds
 	EmitSoundOn( sound_cast, self:GetCaster() )
+end
+
+function ability_thunder_clap:MakeBlockers_old(caster)
+    local block_width = 24
+	local block_delta = 8.25
+
+    -- Create blocker along path
+	--local block_spacing = (block_delta+2*block_width)
+	local blocks = 8 --distance/block_spacing
+	--local block_pos = caster:GetHullRadius() + block_delta + block_width
+	--local start_pos = caster:GetOrigin() + direction*block_pos
+
+	for i=1,blocks do
+        local progress = i/blocks
+        local theta = -2 * math.pi * progress
+		local x =  math.sin( theta ) * block_width * 0.5
+		local y = -math.cos( theta ) * block_delta * 0.5
+        local block_vec = Vector(caster:GetAbsOrigin().x + x, caster:GetAbsOrigin().y + y, caster:GetAbsOrigin().z)
+		--local block_vec = caster:GetOrigin() + direction*block_pos
+        local blocker_unit = CreateUnitByName("npc_avalore_thunder_clap_blocker", block_vec, false, nil, nil, caster:GetTeam())
+		--local blocker = CreateModifierThinker(
+        -- local blocker = blocker_unit:AddNewModifier(
+		-- 	caster, -- player source
+		-- 	self, -- ability source
+		-- 	"modifier_erect_wall_thinker", -- modifier name
+		-- 	{ duration = duration } -- kv
+        -- )
+			--block_vec,
+			--caster:GetTeamNumber(),
+			--true
+		--)
+		--blocker:SetHullRadius( block_width )
+		--block_pos = block_pos + block_spacing
+	end
+end
+
+function ability_thunder_clap:MakeBlockers(caster)
+    local block_width = 24
+	local block_delta = 8.25
+    local blocks = 8
+
+    local radius = self:GetSpecialValueFor("radius")
+    local caster_loc = caster:GetAbsOrigin()
+    local caster_facing = caster:GetForwardVector()
+
+    -- we're going to spawn a semi-circle, so we want to start on direct left/right of the caster
+    -- then iteratively move in a semi-circle spawning rocks/blockers
+    --local vX = caster_loc.x + caster_facing.x * radius
+    -- local curr_block_vec = Vector(  caster_loc.x + caster_facing.x * radius,
+    --                                 caster_loc.y,
+    --                                 caster_loc.z)
+
+    local curr_block_vec = GetGroundPosition(caster_loc + Vector(0, radius, 0), nil)
+
+    for i=1,blocks do
+        local blocker_unit = CreateUnitByName("npc_avalore_thunder_clap_blocker", curr_block_vec, false, nil, nil, caster:GetTeam())
+        curr_block_vec		= RotatePosition(caster_loc, QAngle(0, 180 / blocks, 0), curr_block_vec)
+    end
 end
