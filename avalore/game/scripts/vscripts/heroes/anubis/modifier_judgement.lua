bf51c718d19f034bd154318f1398b42fa925a943
Axosh <axosh.games@gmail.com>
2022-02-24 19:56:16 -0600

    Anubis - Guardian of the Scales
    - core logic complete

--- avalore/game/scripts/vscripts/heroes/anubis/modifier_judgement.lua
+++ avalore/game/scripts/vscripts/heroes/anubis/modifier_judgement.lua
@@ -13,18 +13,57 @@ function modifier_judgement:OnCreated(kv)
 
     if not IsServer() then return end
     self.damage_taken_counter = 1 -- default to healing if neither happens
-    self.damage_given_count = 0
+    self.damage_given_counter = 0
 end
 
 function modifier_judgement:DeclareFunctions()
     return {
         MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE,
-        MODIFIER_PROPERTY_DAMAGEOUTGOING_PERCENTAGE
+        MODIFIER_PROPERTY_TOTALDAMAGEOUTGOING_PERCENTAGE
     }
 end
 
 function modifier_judgement:GetModifierIncomingDamage_Percentage(kv)
-    if keys.attacker and self:GetRemainingTime() >= 0 then
-        self.damage_taken_counter = self.damage_taken_counter + keys.damage
+    if kv.attacker and self:GetRemainingTime() >= 0 then
+        self.damage_taken_counter = self.damage_taken_counter + kv.damage
+        --print("Damage Taken => " .. tostring(self.damage_taken_counter))
+    end
+end
+
+function modifier_judgement:GetModifierTotalDamageOutgoing_Percentage(kv)
+    if not IsServer() then return end
+
+    if kv.attacker == self:GetParent() and bit.band(kv.damage_flags, DOTA_DAMAGE_FLAG_REFLECTION) ~= DOTA_DAMAGE_FLAG_REFLECTION then
+        --self.damage_given_count = self.damage_given_count + 
+        --print("OUTGOING DAMAGE ====================")
+        --PrintTable(kv)
+        self.damage_given_counter = self.damage_given_counter + kv.original_damage
+    end
+end
+
+function modifier_judgement:OnDestroy()
+    if not IsServer() then return end
+
+    if self.damage_taken_counter > self.damage_given_counter then
+        -- if this is imba, then maybe consider doing (taken/given) * max_heal
+        self:GetParent():EmitSound("Hero_Oracle.FalsePromise.Healed")
+        self.end_particle = ParticleManager:CreateParticle("particles/units/heroes/hero_oracle/oracle_false_promise_heal.vpcf", PATTACH_ABSORIGIN_FOLLOW, self:GetParent())
+		ParticleManager:ReleaseParticleIndex(self.end_particle)
+        self:GetParent():Heal(self.max_heal, self:GetCaster())
+    else
+        -- if this is imba, then maybe consider doing (given/taken) * max_dmg
+        self:GetParent():EmitSound("Hero_Oracle.FalsePromise.Damaged")
+        self.end_particle = ParticleManager:CreateParticle("particles/units/heroes/hero_oracle/oracle_false_promise_dmg.vpcf", PATTACH_ABSORIGIN_FOLLOW, self:GetParent())
+		ParticleManager:ReleaseParticleIndex(self.end_particle)
+        local damageTable = {
+            attacker        = self:GetCaster(),
+            victim          = self:GetParent(),
+            damage          = self.max_dmg,
+            damage_type     = DAMAGE_TYPE_PURE,
+            damage_flags    = DOTA_DAMAGE_FLAG_NONE,
+            ability         = self:GetAbility()
+        }
+        ApplyDamage( damageTable )
+        print("dealt damage")
     end
 end
