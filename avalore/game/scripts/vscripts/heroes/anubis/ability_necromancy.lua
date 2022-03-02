9220c89d7b5e3ccf8d5cd345a28672168b6d323f
Axosh <axosh.games@gmail.com>
2022-03-01 21:21:20 -0600

    Anubis - Necromancy [WIP]
    - filled in the core of the ability that's needed for basic testing

--- avalore/game/scripts/vscripts/heroes/anubis/ability_necromancy.lua
+++ avalore/game/scripts/vscripts/heroes/anubis/ability_necromancy.lua
@@ -2,6 +2,7 @@ ability_necromancy = class({})
 
 LinkLuaModifier("modifier_necromancy_aura", "heroes/anubis/modifier_necromancy_aura", LUA_MODIFIER_MOTION_NONE)
 
+
 function ability_necromancy:GetIntrinsicModifierName()
     return "modifier_necromancy_aura"
 end
--- avalore/game/scripts/vscripts/heroes/anubis/modifier_necromancy_aura_buff_form.lua
+++ avalore/game/scripts/vscripts/heroes/anubis/modifier_necromancy_aura_buff_form.lua
@@ -1,5 +1,55 @@
 modifier_necromancy_aura_buff_form = modifier_necromancy_aura_buff_form or class({})
 
-function modifier_necromancy_aura_buff_form:OnCreated()
 
+function modifier_necromancy_aura_buff_form:IsHidden() return false end
+function modifier_necromancy_aura_buff_form:IsDebuff() return false end
+function modifier_necromancy_aura_buff_form:IsPurgable() return false end
+
+function modifier_necromancy_aura_buff_form:OnCreated()
+    elf.caster = self:GetCaster()
+	self.ability = self:GetAbility()
+	self.parent = self:GetParent()
+end
+
+function modifier_necromancy_aura_buff_form:DeclareFunctions()
+	return {    MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_MAGICAL,
+				MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PHYSICAL,
+				MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PURE,
+				MODIFIER_PROPERTY_DISABLE_HEALING,
+				MODIFIER_PROPERTY_MODEL_SCALE,
+				MODIFIER_EVENT_ON_TAKEDAMAGE }
+end
+
+function modifier_necromancy_aura_buff_form:CheckState()
+	local state = {[MODIFIER_STATE_NO_HEALTH_BAR] = true,
+				   [MODIFIER_STATE_NO_UNIT_COLLISION] = true,
+				   [MODIFIER_STATE_FLYING_FOR_PATHING_PURPOSES_ONLY] = true}
+	return state
+end
+
+
+function modifier_necromancy_aura_buff_form:GetAbsoluteNoDamageMagical()
+	return 1
+end
+
+function modifier_necromancy_aura_buff_form:GetAbsoluteNoDamagePhysical()
+	return 1
+end
+
+function modifier_necromancy_aura_buff_form:GetAbsoluteNoDamagePure()
+	return 1
+end
+
+function modifier_necromancy_aura_buff_form:GetDisableHealing()
+	return 1
+end
+
+function modifier_necromancy_aura_buff_form:GetModifierModelScale()
+	return 105
+end
+
+
+
+function modifier_necromancy_aura_buff_form:GetStatusEffectName()
+	return "particles/status_fx/status_effect_wraithking_ghosts.vpcf"
 end
