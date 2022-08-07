dccae02cee341c9bbbb9c75c2f9da5622debf79c
Axosh <axosh.games@gmail.com>
2022-08-07 12:55:59 -0500

    Anubis - Epitaph Spells
    - think this should be complete now

--- avalore/game/scripts/vscripts/heroes/anubis/modifier_tomb_aura.lua
+++ avalore/game/scripts/vscripts/heroes/anubis/modifier_tomb_aura.lua
@@ -42,6 +42,9 @@ function modifier_tomb_aura:OnCreated()
     --     self.radius = self.radius + 
     -- end
     --self.radius = self:GetAbility():GetCastRange()
+    if self:GetCaster():GetOwner() then
+        print("Owner => " .. self:GetCaster():GetOwner():GetName())
+    end
 
 --    if not IsServer() then return end
     local aura_particle = ParticleManager:CreateParticle("particles/econ/items/enigma/enigma_world_chasm/enigma_blackhole_ti5_ring_spiral.vpcf", PATTACH_ABSORIGIN_FOLLOW, self:GetParent())
@@ -55,6 +58,83 @@ function modifier_tomb_aura:OnCreated()
 	ParticleManager:SetParticleControl(aura_particle, 1, Vector(self.radius, self.radius, 1))
     
 	self:AddParticle(aura_particle, false, false, -1, false, false)
+
+    -- add a visual if they have the talent
+    if self:GetCaster():GetOwner():HasTalent("talent_epitaph_spells") then
+        --aura_particle = ParticleManager:CreateParticle("particles/econ/items/dazzle/dazzle_ti6/dazzle_ti6_shallow_grave_glyph.vpcf", PATTACH_ABSORIGIN_FOLLOW, self:GetParent())
+        aura_particle = ParticleManager:CreateParticle("particles/econ/items/earthshaker/earthshaker_arcana/earthshaker_arcana_echoslam_start_v2_glyphs.vpcf", PATTACH_ABSORIGIN_FOLLOW, self:GetParent())
+        ParticleManager:SetParticleAlwaysSimulate(aura_particle)
+        ParticleManager:SetParticleControl(aura_particle, 0, self:GetParent():GetAbsOrigin())
+	    ParticleManager:SetParticleControl(aura_particle, 1, Vector(self.radius, self.radius, 1))
+        self.epitaph_fx = aura_particle
+
+
+        --aura_particle = ParticleManager:CreateParticle("particles/units/heroes/hero_dazzle/dazzle_shallow_grave_glyph.vpcff", PATTACH_ABSORIGIN_FOLLOW, self:GetParent())
+        aura_particle = ParticleManager:CreateParticle("particles/econ/items/dazzle/dazzle_ti6/dazzle_ti6_shallow_grave_glyph.vpcf", PATTACH_ABSORIGIN_FOLLOW, self:GetParent())
+        ParticleManager:SetParticleControl(aura_particle, 0, self:GetParent():GetAbsOrigin())
+	    ParticleManager:SetParticleControl(aura_particle, 1, Vector(self.radius, self.radius, 1))
+        
+    end
+end
+
+function modifier_tomb_aura:OnDestroy()
+    if self.epitaph_fx then
+        ParticleManager:DestroyParticle(self.epitaph_fx, true)
+    end
 end
 
 --function modifier_tomb_aura:
+
+-- this is so you can't literally spawn the tomb on top of someone and keep them trapped under it
+function modifier_tomb_aura:CheckState()
+	return {
+	    [MODIFIER_STATE_NO_UNIT_COLLISION] = true,
+	}
+end
+
+function modifier_tomb_aura:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_MAGICAL,
+		MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PHYSICAL,
+		MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PURE,
+		MODIFIER_EVENT_ON_ATTACK_LANDED,
+	}
+end
+
+function modifier_tomb_aura:GetAbsoluteNoDamageMagical()
+	return 1
+end
+
+function modifier_tomb_aura:GetAbsoluteNoDamagePhysical()
+	return 1
+end
+
+function modifier_tomb_aura:GetAbsoluteNoDamagePure()
+	return 1
+end
+
+function modifier_tomb_aura:OnAttackLanded(kv)
+    -- make sure this is the tomb being attacked
+    if kv.target == self:GetCaster() then
+        local damage = 1 --creep damage
+        if kv.attacker:IsRealHero() then
+            damage = 4 --hero damage
+        end
+
+        --apply damage
+        -- check if we need to kill tomb
+		if (self:GetCaster():GetHealth() - damage) <= 0 then
+            -- deal damage to the owner for failing to "protect" this tomb
+            if IsServer() then
+                local player_owner = self:GetCaster():GetOwner()
+                player_owner:SetHealth(player_owner:GetHealth() - 400)
+            end
+
+			self:GetCaster():Kill(nil, kv.attacker)
+			self:Destroy()
+		else
+			-- If the damage would not kill it, then simply reduce the Tombstone's health.
+			self:GetCaster():SetHealth(self:GetCaster():GetHealth() - damage)
+		end
+    end
+end
