a4bf0acba35f9c574e216f59991b80f6a95c0193
Axosh <axosh.games@gmail.com>
2022-09-17 22:02:06 -0500

    Item - Fairy Dust [WIP]
    - use existing modifier

--- avalore/game/scripts/vscripts/items/shop/base_materials/item_fairy_dust.lua
+++ avalore/game/scripts/vscripts/items/shop/base_materials/item_fairy_dust.lua
@@ -1,9 +1,9 @@
 item_fairy_dust = class({})
 
-LinkLuaModifier( "modifier_avalore_sky_walk", "modifiers/base_spell/modifier_avalore_sky_walk.lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_avalore_flying", "modifiers/modifier_avalore_flying.lua", LUA_MODIFIER_MOTION_NONE )
 
 function item_fairy_dust:OnSpellStart()
-    self:GetCaster():AddNewModifier(self:GetCaster(), self, "modifier_avalore_sky_walk", {duration = self:GetSpecialValueFor("flying_time")})
+    self:GetCaster():AddNewModifier(self:GetCaster(), self, "modifier_avalore_flying", {duration = self:GetSpecialValueFor("flying_time")})
 end
 
 
