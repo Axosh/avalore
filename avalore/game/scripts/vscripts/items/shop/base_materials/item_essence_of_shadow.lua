3f6f53dc257b713010924fa717e235832b5b7756
Axosh <axosh.games@gmail.com>
2022-10-14 11:05:56 -0500

    Item Work + EoS [WIP]

--- avalore/game/scripts/vscripts/items/shop/base_materials/item_essence_of_shadow.lua
+++ avalore/game/scripts/vscripts/items/shop/base_materials/item_essence_of_shadow.lua
@@ -22,8 +22,20 @@ end
 function modifier_item_essence_of_shadow:OnCreated(event)
     self.invis_fade = self:GetAbility():GetSpecialValueFor("invis_delay")
     
-    local particle = ParticleManager:CreateParticle("particles/generic_hero_status/status_invisibility_start.vpcf", PATTACH_ABSORIGIN, self:GetParent())
-    ParticleManager:ReleaseParticleIndex(particle)
+    -- local particle = ParticleManager:CreateParticle("particles/generic_hero_status/status_invisibility_start.vpcf", PATTACH_ABSORIGIN, self:GetParent())
+    -- ParticleManager:ReleaseParticleIndex(particle)
     self:SetStackCount(0) -- start out invis when trigger the spell
     self:StartIntervalThink( 1 )
+end
+
+-- break on right-click attack
+function modifier_item_essence_of_shadow:OnAttack(keys)
+    if not IsServer() then return end
+
+    local attacker = keys.attacker
+    -- Only apply if the parent is the one attacking
+    if self:GetParent() == attacker then
+        self:SetStackCount(self.cooldown)
+    end
+    
 end
