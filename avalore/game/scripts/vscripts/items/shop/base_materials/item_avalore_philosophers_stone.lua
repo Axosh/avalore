abfa4dd8c5c33e96e22ad172b3d5a5dc090a7c82
Axosh <axosh.games@gmail.com>
2022-11-20 19:05:58 -0600

    Item - Philosopher's Stone [Fixed]
    Fixed collision between dota neutral item and avalore version

---
+++ avalore/game/scripts/vscripts/items/shop/base_materials/item_avalore_philosophers_stone.lua
@@ -0,0 +1,42 @@
+item_avalore_philosophers_stone = class({})
+
+LinkLuaModifier( "modifier_item_avalore_philosophers_stone", "items/shop/base_materials/item_avalore_philosophers_stone.lua", LUA_MODIFIER_MOTION_NONE )
+
+function item_avalore_philosophers_stone:GetIntrinsicModifierName()
+    return "modifier_item_avalore_philosophers_stone"
+end
+
+-- ====================================
+-- INTRINSIC MOD
+-- ====================================
+
+modifier_item_avalore_philosophers_stone = modifier_item_avalore_philosophers_stone or class({})
+
+function modifier_item_avalore_philosophers_stone:IsHidden()      return true  end
+function modifier_item_avalore_philosophers_stone:IsDebuff()      return false end
+function modifier_item_avalore_philosophers_stone:IsPurgable()    return false end
+function modifier_item_avalore_philosophers_stone:RemoveOnDeath() return false end
+
+function modifier_item_avalore_philosophers_stone:DeclareFunctions()
+    return { MODIFIER_PROPERTY_TOOLTIP }
+end
+
+function modifier_item_avalore_philosophers_stone:OnCreated(kv)
+    self.item_ability       = self:GetAbility()
+    self.gold_per_min       = self.item_ability:GetSpecialValueFor("gold_per_min")
+    self.gold_per_tick      = self.gold_per_min / 30
+
+    if not IsServer() then return end
+    self:StartIntervalThink(2)
+end
+
+function modifier_item_avalore_philosophers_stone:OnTooltip()
+    return self.gold_per_min
+end
+
+function modifier_item_avalore_philosophers_stone:OnIntervalThink()
+    if not IsServer() then return end
+
+    -- careful with this because gold_per_tick needs to be type "INT" or it takes the floor
+    self:GetParent():ModifyGold(self.gold_per_tick, false, DOTA_ModifyGold_AbilityGold)
+end
