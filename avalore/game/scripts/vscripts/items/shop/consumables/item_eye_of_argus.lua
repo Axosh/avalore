7183bf46f0721d2feb780b10703af58464f68bc1
Axosh <axosh.games@gmail.com>
2023-01-13 18:24:52 -0600

    Tinkering with Wards some more

--- avalore/game/scripts/vscripts/items/shop/consumables/item_eye_of_argus.lua
+++ avalore/game/scripts/vscripts/items/shop/consumables/item_eye_of_argus.lua
@@ -6,38 +6,54 @@ function item_eye_of_argus:GetBehavior()
     return DOTA_ABILITY_BEHAVIOR_POINT + DOTA_ABILITY_BEHAVIOR_OPTIONAL_UNIT_TARGET
 end
 
--- function item_eye_of_argus:CastFilterResultLocation(vLoc)
---     if IsClient() then
---         if self.custom_indicator then
--- 			-- register cursor position
--- 			self.custom_indicator:Register( vLoc )
--- 		end
--- 	end
 
--- 	return UF_SUCCESS
--- end
 
--- function item_eye_of_argus:CreateCustomIndicator()
---     local particle = "particles/ui_mouseactions/range_finder_generic_ward_aoe.vpcf"
---     local radius = self:GetSpecialValueFor("vision_range")
---     self.effect_indicator = ParticleManager:CreateParticle( particle, PATTACH_CUSTOMORIGIN, self:GetCaster())
--- 	ParticleManager:SetParticleControl( self.effect_indicator, 2, Vector( radius, radius, radius ) )
--- end
+function item_eye_of_argus:CastFilterResultLocation(vLoc)
+    if IsClient() then
+        if self.custom_indicator then
+			-- register cursor position
+			self.custom_indicator:Register( vLoc )
+		end
+	end
 
--- function item_eye_of_argus:UpdateCustomIndicator( loc )
--- 	-- update particle position
--- 	ParticleManager:SetParticleControl( self.effect_indicator, 0, loc )
--- end
+	return UF_SUCCESS
+end
 
--- function item_eye_of_argus:DestroyCustomIndicator()
--- 	-- destroy particle
--- 	ParticleManager:DestroyParticle( self.effect_indicator, false )
--- 	ParticleManager:ReleaseParticleIndex( self.effect_indicator )
--- end
 
--- function item_eye_of_argus:GetIntrinsicModifierName()
--- 	return "modifier_generic_custom_indicator"
--- end
+-- Reference Code: https://github.com/SteamDatabase/GameTracking-Dota2/blob/master/game/dota_addons/winter2022/scripts/vscripts/modifiers/gameplay/modifier_building_candy_bucket.lua#L280
+function item_eye_of_argus:CreateCustomIndicator()
+    local particle = "particles/ui_mouseactions/range_finder_generic_ward_aoe.vpcf"
+    local radius = self:GetSpecialValueFor("vision_range")
+    self.effect_indicator = ParticleManager:CreateParticle( particle, PATTACH_CUSTOMORIGIN, self:GetCaster())
+	ParticleManager:SetParticleControl( self.effect_indicator, 0, self:GetCaster():GetAbsOrigin() )
+    --ParticleManager:SetParticleControl( self.effect_indicator, 1, Vector( radius, radius, radius ) )
+    --ParticleManager:SetParticleControl( self.effect_indicator, 2, Vector( radius, radius, radius ) )
+    ParticleManager:SetParticleControl( self.effect_indicator, 1, self:GetCaster():GetAbsOrigin())
+    ParticleManager:SetParticleControl( self.effect_indicator, 2, self:GetCaster():GetAbsOrigin())
+    ParticleManager:SetParticleControl( self.effect_indicator, 3, Vector( radius, radius, radius ))
+    ParticleManager:SetParticleControl( self.effect_indicator, 6, Vector( 250, 0, 60 ) );
+    ParticleManager:SetParticleControl( self.effect_indicator, 10, self:GetCaster():GetAbsOrigin())
+    ParticleManager:SetParticleControl( self.effect_indicator, 11, Vector( radius, radius, radius ))
+
+end
+
+function item_eye_of_argus:UpdateCustomIndicator( loc )
+	-- update particle position
+	ParticleManager:SetParticleControl( self.effect_indicator, 0, loc )
+    ParticleManager:SetParticleControl( self.effect_indicator, 1, loc )
+    ParticleManager:SetParticleControl( self.effect_indicator, 2, loc )
+    ParticleManager:SetParticleControl( self.effect_indicator, 10, loc )
+end
+
+function item_eye_of_argus:DestroyCustomIndicator()
+	-- destroy particle
+	ParticleManager:DestroyParticle( self.effect_indicator, false )
+	ParticleManager:ReleaseParticleIndex( self.effect_indicator )
+end
+
+function item_eye_of_argus:GetIntrinsicModifierName()
+	return "modifier_generic_custom_indicator"
+end
 
 function item_eye_of_argus:OnSpellStart()
     local caster = self:GetCaster()
