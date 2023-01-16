47ddb2c3365db73e64e5b0718f7c08286f41d770
Axosh <axosh.games@gmail.com>
2023-01-16 17:04:40 -0600

    Panorama UI work

---
+++ avalore/content/panorama/scripts/custom_game/avalore_overrides.js
@@ -0,0 +1,26 @@
+// idk if this will work, but trying to override the default funciton of the stash
+function DOTAHUDStashGrabAll() {
+    $.Msg( "Hello from DOTAHUDStashGrabAll, World!" );
+    var unit = Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer());
+    if( unit != -1) {
+        GameEvents.SendCustomGameEventToServer("Avalore_Take_Stash", { 'entindex':unit});
+    }
+}
+
+function AVALOREHUDStashGrabAll() {
+    $.Msg( "Hello from AVALOREHUDStashGrabAll, World!" );
+    var unit = Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer());
+    if( unit != -1) {
+        GameEvents.SendCustomGameEventToServer("Avalore_Take_Stash", { 'entindex':unit});
+    }
+}
+
+(function() {
+	$.Msg( "Hello from avalore_overrides, World! asdf" );
+
+    $("#grab_all_button").SetPanelEvent("onactivate", function() { 
+        AVALOREHUDStashGrabAll();
+     });
+
+	//Init();
+})();
