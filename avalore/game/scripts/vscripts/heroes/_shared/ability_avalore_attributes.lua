ee6ff8a26c165e0b2a2d9bea1a7733f1bff0dcbe
Axosh <axosh.games@gmail.com>
2023-02-07 11:13:46 -0600

    Attributes Ability Added

--- avalore/game/scripts/vscripts/heroes/_shared/ability_avalore_attributes.lua
+++ avalore/game/scripts/vscripts/heroes/_shared/ability_avalore_attributes.lua
@@ -1,7 +1,15 @@
 ability_avalore_attributes = ability_avalore_attributes or class({})
 
-function ability_avalore_attributes:GetIntrinsicModifierName()
-    return "modifier_avalore_attributes"
+LinkLuaModifier("modifier_avalore_attributes",   "scripts/vscripts/heroes/_shared/ability_avalore_attributes.lua", LUA_MODIFIER_MOTION_NONE)
+
+-- function ability_avalore_attributes:GetIntrinsicModifierName()
+--     return "modifier_avalore_attributes"
+-- end
+
+function ability_avalore_attributes:OnUpgrade()
+    --print("Attributes Leveled")
+    --self:GetCaster():FindModifierByName("modifier_avalore_attributes"):ForceRefresh()
+    self:GetCaster():AddNewModifier(self:GetCaster(), self, "modifier_avalore_attributes", {})
 end
 
 -- ====================================
@@ -13,9 +21,9 @@ function modifier_avalore_attributes:IsHidden() return true end
 function modifier_avalore_attributes:IsDebuff() return false end
 function modifier_avalore_attributes:IsPurgable() return false end
 function modifier_avalore_attributes:RemoveOnDeath() return false end
-function modifier_avalore_attributes:GetAttributes()
-    return MODIFIER_ATTRIBUTE_MULTIPLE
-end
+-- function modifier_avalore_attributes:GetAttributes()
+--     return MODIFIER_ATTRIBUTE_MULTIPLE
+-- end
 
 function modifier_avalore_attributes:DeclareFunctions()
     return {    MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE,
@@ -25,4 +33,79 @@ function modifier_avalore_attributes:DeclareFunctions()
                 MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
                 MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
                 MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT }
+end
+
+function modifier_avalore_attributes:OnCreated()
+    --print("modifier_avalore_attributes:OnCreated()")
+    self.raw_dmg        = self:GetAbility():GetSpecialValueFor("raw_dmg")
+    self.raw_ms         = self:GetAbility():GetSpecialValueFor("raw_ms")
+    self.minor_stats    = self:GetAbility():GetSpecialValueFor("minor_stats")
+    self.attack_speed   = self:GetAbility():GetSpecialValueFor("attack_speed")
+    self.mana_regen     = self:GetAbility():GetSpecialValueFor("mana_regen")
+    self.major_stats    = self:GetAbility():GetSpecialValueFor("major_stats")
+
+    self.bonus_dmg      = 0
+    self.bonus_ms       = 0
+    self.bonus_stats    = 0
+    self.bonus_as       = 0
+    self.bonus_regen    = 0
+
+    self:OnRefresh()
+end
+
+function modifier_avalore_attributes:OnRefresh()
+    local ability_level = self:GetAbility():GetLevel()
+    --print("Refreshing Attributes - level " .. tostring(ability_level))
+
+    if ability_level > 0 then
+        self.bonus_dmg = self.raw_dmg
+    end
+
+    if ability_level > 1 then
+        self.bonus_ms = self.raw_ms
+    end
+
+    if ability_level > 2 then
+        self.bonus_stats = self.minor_stats
+    end
+
+    if ability_level > 3 then
+        self.bonus_as = self.attack_speed
+    end
+
+    if ability_level > 4 then
+        self.bonus_regen = self.mana_regen
+    end
+
+    if ability_level > 5 then
+        self.bonus_stats = self.bonus_stats + self.major_stats
+    end
+end
+
+function modifier_avalore_attributes:GetModifierPreAttack_BonusDamage()
+    return self.bonus_dmg
+end
+
+function modifier_avalore_attributes:GetModifierMoveSpeedBonus_Constant()
+    return self.bonus_ms
+end
+
+function modifier_avalore_attributes:GetModifierConstantManaRegen()
+    return self.bonus_regen
+end
+
+function modifier_avalore_attributes:GetModifierBonusStats_Strength()
+    return self.bonus_stats
+end
+
+function modifier_avalore_attributes:GetModifierBonusStats_Agility()
+    return self.bonus_stats
+end
+
+function modifier_avalore_attributes:GetModifierBonusStats_Intellect()
+    return self.bonus_stats
+end
+
+function modifier_avalore_attributes:GetModifierAttackSpeedBonus_Constant()
+    return self.bonus_as
 end
