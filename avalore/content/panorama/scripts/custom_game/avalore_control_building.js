d2ae58684783a3a75471ad63d22976140ee7f56b
Axosh <axosh.games@gmail.com>
2023-02-28 16:44:21 -0600

    Control Building Button

--- avalore/content/panorama/scripts/custom_game/avalore_control_building.js
+++ avalore/content/panorama/scripts/custom_game/avalore_control_building.js
@@ -2,18 +2,27 @@ function OnUpdateSelectedUnit() {
     var mainSelected = Players.GetLocalPlayerPortraitUnit()
     var unitName = Entities.GetUnitName(mainSelected)
     $.Msg("Selected Unit => " + unitName)
+    // If not a building a user can control
     if (unitName.indexOf("mercenary_camp") <= -1 && unitName.indexOf("building_arcanery") <= -1) {
+        $.Msg("Should Hide")
         $("#TakeControlDisplay").SetHasClass("Hidden", true);
         //$(".ControlBuildingContainer").hittest = false;
     }
     else {
+        $.Msg("VISIBLE")
         $("#TakeControlDisplay").SetHasClass("Hidden", false);
         //$(".ControlBuildingContainer").hittest = true;
     }
+}
+
+function OnAbilityUsed() {
+    $.Msg("OnAbilityUsed")
 }
 
 // wireup string-id to javascript function
 (function()
 {
 	GameEvents.Subscribe( "dota_player_update_selected_unit", OnUpdateSelectedUnit );
+    GameEvents.Subscribe( "dota_player_update_query_unit", OnUpdateSelectedUnit );
+    GameEvents.Subscribe( "dota_player_used_ability", OnAbilityUsed)
 })();
