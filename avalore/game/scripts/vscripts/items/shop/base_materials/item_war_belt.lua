c67f2feb3f913a6b0c5701518ad2b38daf5964cb
Axosh <axosh.games@gmail.com>
2023-04-14 14:55:06 -0500

    Item - War Belt [Rework]

--- avalore/game/scripts/vscripts/items/shop/base_materials/item_war_belt.lua
+++ avalore/game/scripts/vscripts/items/shop/base_materials/item_war_belt.lua
@@ -18,14 +18,20 @@ function modifier_item_war_belt:IsPurgable() return false end
 function modifier_item_war_belt:RemoveOnDeath() return false end
 
 function modifier_item_war_belt:DeclareFunctions()
-    return {    MODIFIER_PROPERTY_STATS_STRENGTH_BONUS }
+    return {    MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
+                MODIFIER_PROPERTY_STATS_AGILITY_BONUS }
 end
 
 function modifier_item_war_belt:OnCreated(event)
     self.item_ability = self:GetAbility()
     self.bonus_str = self.item_ability:GetSpecialValueFor("bonus_str")
+    self.bonus_agi = self.item_ability:GetSpecialValueFor("bonus_agi")
 end
 
 function modifier_item_war_belt:GetModifierBonusStats_Strength()
     return self.bonus_str
+end
+
+function modifier_item_war_belt:GetModifierBonusStats_Agility()
+    return self.bonus_agi
 end
