0bd8d6924f22af1fe21676e7af248f4a1528c695
Axosh <axosh.games@gmail.com>
2023-07-04 11:06:38 -0500

    Item - Mantle of Invisibility

--- avalore/game/scripts/vscripts/items/shop/tier3/item_mantle_of_invisibility.lua
+++ avalore/game/scripts/vscripts/items/shop/tier3/item_mantle_of_invisibility.lua
@@ -1,9 +1,29 @@
 item_mantle_of_invisiibility = class({})
 
 LinkLuaModifier( "modifier_item_mantle_of_invisiibility", "items/shop/tier3/item_mantle_of_invisiibility.lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_item_mantle_of_invisiibility_invis", "items/shop/tier3/item_mantle_of_invisiibility.lua", LUA_MODIFIER_MOTION_NONE )
 
 function item_mantle_of_invisiibility:GetIntrinsicModifierName()
     return "modifier_item_mantle_of_invisiibility"
+end
+
+function item_mantle_of_invisiibility:OnSpellStart()
+    local caster    =   self:GetCaster()
+	local particle_invis_start = "particles/generic_hero_status/status_invisibility_start.vpcf"
+
+    local duration  =   self:GetSpecialValueFor("invis_duration")
+	local fade_time =   self:GetSpecialValueFor("invis_fade_time")
+
+    EmitSoundOn("DOTA_Item.InvisibilitySword.Activate", self:GetCaster())
+
+    Timers:CreateTimer(fade_time, function()
+		local particle_invis_start_fx = ParticleManager:CreateParticle(particle_invis_start, PATTACH_ABSORIGIN, caster)
+		ParticleManager:SetParticleControl(particle_invis_start_fx, 0, caster:GetAbsOrigin())
+		ParticleManager:ReleaseParticleIndex(particle_invis_start_fx)
+
+		caster:AddNewModifier(caster, self, "modifier_item_mantle_of_invisiibility_invis", {duration = duration})
+	end)
+
 end
 
 -- ====================================
@@ -60,4 +80,63 @@ end
 
 function modifier_item_mantle_of_invisiibility:GetModifierMoveSpeedBonus_Constant()
     return self.bonus_move_speed
+end
+
+-- ====================================
+-- ACTIVE MOD
+-- ====================================
+
+modifier_item_mantle_of_invisiibility_invis = class({})
+
+function modifier_item_mantle_of_invisiibility_invis:IsDebuff() return false end
+function modifier_item_mantle_of_invisiibility_invis:IsHidden() return false end
+function modifier_item_mantle_of_invisiibility_invis:IsPurgable() return false end
+
+function modifier_item_mantle_of_invisiibility_invis:OnCreated(params)
+    if not self:GetAbility() then self:Destroy() return end
+    self.invis_ms_pct = self.item_ability:GetSpecialValueFor("invis_ms_pct")
+    
+end
+
+function modifier_item_mantle_of_invisiibility_invis:CheckState()
+	return {
+		[MODIFIER_STATE_INVISIBLE]			= true,
+        [MODIFIER_STATE_NO_UNIT_COLLISION] = true
+	}
+end
+
+function modifier_item_mantle_of_invisiibility_invis:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_INVISIBILITY_LEVEL,
+		MODIFIER_PROPERTY_TRANSLATE_ACTIVITY_MODIFIERS,
+        MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE,
+        MODIFIER_EVENT_ON_ATTACK,
+		MODIFIER_EVENT_ON_ABILITY_EXECUTED
+	}
+end
+
+function modifier_item_mantle_of_invisiibility_invis:GetModifierInvisibilityLevel()
+	return 1
+end
+
+function modifier_item_mantle_of_invisiibility_invis:GetModifierMoveSpeedBonus_Percentage()
+    return self.invis_ms_pct
+end
+
+function modifier_item_mantle_of_invisiibility_invis:OnAttack(params)
+	if IsServer() then
+		if params.attacker == self:GetParent() then
+			self:Destroy()
+		end
+	end
+end
+
+function modifier_item_mantle_of_invisiibility_invis:OnAbilityExecuted( keys )
+	if IsServer() then
+		local parent =	self:GetParent()
+		-- Remove the invis on cast
+		if keys.unit == parent then
+			self:Destroy()
+		end
+	end
 end
