223bb655f6a417c48b0d80a0464246230ce22b5e
Axosh <axosh.games@gmail.com>
2023-08-29 18:57:29 -0500

    ul ai

--- avalore/game/scripts/vscripts/units/urban_legends/chainsaw_murderer/ai_chainsaw_murderer.lua
+++ avalore/game/scripts/vscripts/units/urban_legends/chainsaw_murderer/ai_chainsaw_murderer.lua
@@ -8,10 +8,17 @@ function Spawn( entityKeyValues )
     end
 
     thisEntity.isCarrying = false
+    thisEntity.FirstPass = true
 
     thisEntity:SetContextThink("ChainsawMurdererAIThink", ChainsawMurdererAIThink, 1)
+    hHook = thisEntity:FindAbilityByName("pudge_meat_hook")
+    hGrab = thisEntity:FindAbilityByName("ability_ul_grab")
 end
 
 function ChainsawMurdererAIThink( self )
+    if thisEntity.FirstPass then
+		thisEntity.spawnLocation = thisEntity:GetOrigin() -- location still (0,0,0) when in Spawn function, so set it on first pass here
+		thisEntity.FirstPass = false
+	end
 
 end
