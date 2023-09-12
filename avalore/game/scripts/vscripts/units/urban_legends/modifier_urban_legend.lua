a9a4baf39c388478ad54f6f54cbcfcc1f7f038b5
Axosh <axosh.games@gmail.com>
2023-09-11 21:37:42 -0500

    wip - ul modifier

--- avalore/game/scripts/vscripts/units/urban_legends/modifier_urban_legend.lua
+++ avalore/game/scripts/vscripts/units/urban_legends/modifier_urban_legend.lua
@@ -1,1 +1,20 @@
 modifier_urban_legend = class({})
+
+function modifier_urban_legend:DeclareFunctions()
+    return { MODIFIER_EVENT_ON_TAKEDAMAGE }
+end
+
+function modifier_urban_legend:OnTakeDamage(kv)
+
+	if IsServer() then
+		local target = self:GetParent()
+
+		if target == kv.unit then
+            -- if this is gonna kill us
+			if target:GetHealth() - kv.damage <= 0 then
+                
+            end
+		end
+	end
+
+end
