7767586d75c9e270c514bf2a1bd3f6ae12a2e730
Axosh <axosh.games@gmail.com>
2023-09-12 21:57:26 -0500

    ul - minor work

--- avalore/game/scripts/vscripts/units/urban_legends/modifier_urban_legend.lua
+++ avalore/game/scripts/vscripts/units/urban_legends/modifier_urban_legend.lua
@@ -11,10 +11,24 @@ function modifier_urban_legend:OnTakeDamage(kv)
 
 		if target == kv.unit then
             -- if this is gonna kill us
-			if target:GetHealth() - kv.damage <= 0 then
-                
+			if (target:GetHealth() - kv.damage) <= 0 then
+
             end
 		end
 	end
 
 end
+
+-- ==============================================
+-- Invuln/Regen
+-- ==============================================
+modifier_urban_legend_revive = class({})
+
+modifier_urban_legend_revive:DeclareFunctions()
+    return { 
+            MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_MAGICAL, 
+            MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PHYSICAL, 
+            MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PURE,
+            MODIFIER_PROPERTY_DISABLE_HEALING, 
+            MODIFIER_EVENT_ON_ATTACK_LANDED,
+    }
